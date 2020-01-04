d89907f949f215fe968113cb52123d7c17d73128
Eric B. Ridge <eebbrr@gmail.com>
2020-01-04 14:18:15 -0700

    cleanup feature gating

--- pg-bridge/src/fcinfo.rs
+++ pg-bridge/src/fcinfo.rs
@@ -1,172 +1,113 @@
-use crate::{pg_sys, rust_str_to_text_p, DatumCompatible, PgDatum};
+use crate::pg_sys;
 
-//
-// PG 10
-//
+#[cfg(any(feature = "pg10", feature = "pg11"))]
+mod pg_10_11 {
+    use crate::{pg_sys, DatumCompatible, PgDatum};
 
-#[cfg(feature = "pg10")]
-#[inline]
-pub fn pg_getarg<T>(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> PgDatum<T>
-where
-    T: DatumCompatible<T>,
-{
-    PgDatum::<T>::new(
-        unsafe { fcinfo.as_ref() }.unwrap().arg[num],
-        unsafe { fcinfo.as_ref() }.unwrap().argnull[num] as bool,
-    )
-}
-
-#[cfg(feature = "pg10")]
-#[inline]
-pub fn pg_arg_is_null(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> bool {
-    unsafe { fcinfo.as_ref() }.unwrap().argnull[num] as bool
-}
-
-#[cfg(feature = "pg10")]
-#[inline]
-pub fn pg_getarg_datum(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> Option<pg_sys::Datum> {
-    if pg_arg_is_null(fcinfo, num) {
-        None
-    } else {
-        Some(unsafe { fcinfo.as_ref() }.unwrap().arg[num])
+    #[inline]
+    pub fn pg_getarg<T>(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> PgDatum<T>
+    where
+        T: DatumCompatible<T>,
+    {
+        PgDatum::<T>::new(
+            unsafe { fcinfo.as_ref() }.unwrap().arg[num],
+            unsafe { fcinfo.as_ref() }.unwrap().argnull[num] as bool,
+        )
     }
-}
 
-#[cfg(feature = "pg10")]
-#[inline]
-pub fn pg_getarg_datum_raw(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> pg_sys::Datum {
-    unsafe { fcinfo.as_ref() }.unwrap().arg[num]
-}
-
-#[cfg(feature = "pg10")]
-#[inline]
-pub fn pg_return_null(fcinfo: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
-    unsafe { fcinfo.as_mut() }.unwrap().isnull = true;
-    0 as pg_sys::Datum
-}
-
-//
-// PG 11
-//
-
-#[cfg(feature = "pg11")]
-#[inline]
-pub fn pg_getarg<T>(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> PgDatum<T>
-where
-    T: DatumCompatible<T>,
-{
-    PgDatum::<T>::new(
-        unsafe { fcinfo.as_ref() }.unwrap().arg[num],
-        unsafe { fcinfo.as_ref() }.unwrap().argnull[num] as bool,
-    )
-}
-
-#[cfg(feature = "pg11")]
-#[inline]
-pub fn pg_arg_is_null(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> bool {
-    unsafe { fcinfo.as_ref() }.unwrap().argnull[num] as bool
-}
-
-#[cfg(feature = "pg11")]
-#[inline]
-pub fn pg_getarg_datum(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> Option<pg_sys::Datum> {
-    if pg_arg_is_null(fcinfo, num) {
-        None
-    } else {
-        Some(unsafe { fcinfo.as_ref() }.unwrap().arg[num])
+    #[inline]
+    pub fn pg_arg_is_null(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> bool {
+        unsafe { fcinfo.as_ref() }.unwrap().argnull[num] as bool
     }
-}
 
-#[cfg(feature = "pg11")]
-#[inline]
-pub fn pg_getarg_datum_raw(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> pg_sys::Datum {
-    unsafe { fcinfo.as_ref() }.unwrap().arg[num]
-}
+    #[inline]
+    pub fn pg_getarg_datum(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> Option<pg_sys::Datum> {
+        if pg_arg_is_null(fcinfo, num) {
+            None
+        } else {
+            Some(unsafe { fcinfo.as_ref() }.unwrap().arg[num])
+        }
+    }
 
-#[cfg(feature = "pg11")]
-#[inline]
-pub fn pg_return_null(fcinfo: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
-    unsafe { fcinfo.as_mut() }.unwrap().isnull = true;
-    0 as pg_sys::Datum
-}
+    #[inline]
+    pub fn pg_getarg_datum_raw(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> pg_sys::Datum {
+        unsafe { fcinfo.as_ref() }.unwrap().arg[num]
+    }
 
-//
-// PG 12
-//
-
-#[cfg(feature = "pg12")]
-#[inline]
-pub fn pg_getarg<T>(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> PgDatum<T>
-where
-    T: DatumCompatible<T>,
-{
-    let datum = get_nullable_datum(fcinfo, num);
-    PgDatum::<T>::new(datum.value, datum.isnull)
-}
-
-#[cfg(feature = "pg12")]
-#[inline]
-pub fn pg_arg_is_null(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> bool {
-    get_nullable_datum(fcinfo, num).isnull
-}
-
-#[cfg(feature = "pg12")]
-#[inline]
-pub fn pg_getarg_datum(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> Option<pg_sys::Datum> {
-    if pg_arg_is_null(fcinfo, num) {
-        None
-    } else {
-        Some(get_nullable_datum(fcinfo, num).value)
+    #[inline]
+    pub fn pg_return_null(fcinfo: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
+        unsafe { fcinfo.as_mut() }.unwrap().isnull = true;
+        0 as pg_sys::Datum
     }
 }
 
 #[cfg(feature = "pg12")]
-#[inline]
-pub fn pg_getarg_datum_raw(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> pg_sys::Datum {
-    get_nullable_datum(fcinfo, num).value
-}
+mod pg_12 {
+    use crate::{pg_sys, DatumCompatible, PgDatum};
 
-#[cfg(feature = "pg12")]
-#[inline]
-fn get_nullable_datum(
-    fcinfo: pg_sys::FunctionCallInfo,
-    num: usize,
-) -> pg_sys::pg12_specific::NullableDatum {
-    let fcinfo = unsafe { fcinfo.as_mut() }.unwrap();
-    unsafe {
-        let nargs = fcinfo.nargs;
-        let len = std::mem::size_of::<pg_sys::pg12_specific::NullableDatum>() * nargs as usize;
-        fcinfo.args.as_slice(len)[num]
+    #[inline]
+    pub fn pg_getarg<T>(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> PgDatum<T>
+    where
+        T: DatumCompatible<T>,
+    {
+        let datum = get_nullable_datum(fcinfo, num);
+        PgDatum::<T>::new(datum.value, datum.isnull)
     }
-}
 
-#[cfg(feature = "pg12")]
-#[inline]
-pub fn pg_return_null(fcinfo: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
-    let fcinfo = unsafe { fcinfo.as_mut() }.unwrap();
-    fcinfo.isnull = true;
-    0 as pg_sys::Datum
+    #[inline]
+    pub fn pg_arg_is_null(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> bool {
+        get_nullable_datum(fcinfo, num).isnull
+    }
+
+    #[inline]
+    pub fn pg_getarg_datum(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> Option<pg_sys::Datum> {
+        if pg_arg_is_null(fcinfo, num) {
+            None
+        } else {
+            Some(get_nullable_datum(fcinfo, num).value)
+        }
+    }
+
+    #[inline]
+    pub fn pg_getarg_datum_raw(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> pg_sys::Datum {
+        get_nullable_datum(fcinfo, num).value
+    }
+
+    #[inline]
+    fn get_nullable_datum(
+        fcinfo: pg_sys::FunctionCallInfo,
+        num: usize,
+    ) -> pg_sys::pg12_specific::NullableDatum {
+        let fcinfo = unsafe { fcinfo.as_mut() }.unwrap();
+        unsafe {
+            let nargs = fcinfo.nargs;
+            let len = std::mem::size_of::<pg_sys::pg12_specific::NullableDatum>() * nargs as usize;
+            fcinfo.args.as_slice(len)[num]
+        }
+    }
+
+    #[inline]
+    pub fn pg_return_null(fcinfo: pg_sys::FunctionCallInfo) -> pg_sys::Datum {
+        let fcinfo = unsafe { fcinfo.as_mut() }.unwrap();
+        fcinfo.isnull = true;
+        0 as pg_sys::Datum
+    }
 }
 
 //
 // common
 //
 
+#[cfg(any(feature = "pg10", feature = "pg11"))]
+pub use pg_10_11::*;
+
+#[cfg(feature = "pg12")]
+pub use pg_12::*;
+
 #[inline]
 pub fn pg_getarg_pointer<T>(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> Option<*mut T> {
     match pg_getarg_datum(fcinfo, num) {
         Some(datum) => Some(datum as *mut T),
-        None => None,
-    }
-}
-
-#[inline]
-pub fn pg_getarg_boxed<T>(fcinfo: pg_sys::FunctionCallInfo, num: usize) -> Option<crate::PgBox<T>>
-where
-    T: DatumCompatible<T>,
-{
-    match pg_getarg_pointer(fcinfo, num) {
-        Some(ptr) => Some(crate::PgBox::from_pg(ptr)),
         None => None,
     }
 }
@@ -182,11 +123,6 @@ pub fn pg_getarg_cstr<'a>(
         Some(ptr) => Some(unsafe { std::ffi::CStr::from_ptr(ptr) }),
         None => None,
     }
-}
-
-#[inline]
-pub fn pg_return_text_p(s: &str) -> pg_sys::Datum {
-    rust_str_to_text_p(s) as pg_sys::Datum
 }
 
 #[inline]
