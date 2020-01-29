724e09632aa8b97a9d1d6a8f82d749b39565c9d0
Eric B. Ridge <eebbrr@gmail.com>
2020-01-28 18:40:04 -0700

    helper methods

---
+++ pgx/src/utils.rs
@@ -0,0 +1,8 @@
+use crate::pg_sys;
+
+#[inline]
+pub fn name_data_to_str(name_data: &pg_sys::NameData) -> &str {
+    unsafe { std::ffi::CStr::from_ptr(name_data.data.as_ptr()) }
+        .to_str()
+        .unwrap()
+}
