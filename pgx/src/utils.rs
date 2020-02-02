c93cbffec00c43e3b35c763993ddb4f34f69c2da
Eric B. Ridge <eebbrr@gmail.com>
2020-02-02 16:20:06 -0700

    add a lifetime to this

--- pgx/src/utils.rs
+++ pgx/src/utils.rs
@@ -1,7 +1,7 @@
 use crate::pg_sys;
 
 #[inline]
-pub fn name_data_to_str(name_data: &pg_sys::NameData) -> &str {
+pub fn name_data_to_str<'a>(name_data: &pg_sys::NameData) -> &'a str {
     unsafe { std::ffi::CStr::from_ptr(name_data.data.as_ptr()) }
         .to_str()
         .unwrap()
