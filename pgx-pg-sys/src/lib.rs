e655dd7e706b4b3b11eae03122d15d0cc6c22aa6
Eric B. Ridge <eebbrr@gmail.com>
2020-06-24 16:49:28 -0600

    cargo clippy

--- pgx-pg-sys/src/lib.rs
+++ pgx-pg-sys/src/lib.rs
@@ -1,7 +1,6 @@
 // Copyright 2020 ZomboDB, LLC <zombodb@gmail.com>. All rights reserved. Use of this source code is
 // governed by the MIT license that can be found in the LICENSE file.
 
-
 //
 // we allow improper_ctypes just to eliminate these warnings:
 //      = note: `#[warn(improper_ctypes)]` on by default
@@ -123,11 +122,11 @@ mod all_versions {
     ///     ((RangeTblEntry *) list_nth(rangetable, (rangetable_index)-1))
     /// ```
     #[inline]
-    pub fn rt_fetch(
+    pub unsafe fn rt_fetch(
         index: super::Index,
         range_table: *mut super::List,
     ) -> *mut super::RangeTblEntry {
-        unsafe { super::list_nth(range_table, index as i32 - 1) as *mut super::RangeTblEntry }
+        super::list_nth(range_table, index as i32 - 1) as *mut super::RangeTblEntry
     }
 
     #[inline]
