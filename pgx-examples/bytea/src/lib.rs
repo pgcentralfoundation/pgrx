63b3ba98837fb94426e6ded60a0c5fbfd8004eb5
Eric B. Ridge <eebbrr@gmail.com>
2020-07-25 19:59:43 -0700

    cleanup the bytea example a bit

--- pgx-examples/bytea/src/lib.rs
+++ pgx-examples/bytea/src/lib.rs
@@ -4,17 +4,9 @@ use std::io::{Read, Write};
 
 pg_module_magic!();
 
+/// gzip bytes.  Postgres will automatically convert `text`/`varchar` data into `bytea`
 #[pg_extern]
-fn gzip_text(input: &str) -> Vec<u8> {
-    let mut encoder = Encoder::new(Vec::new()).expect("failed to construct gzip Encoder");
-    encoder
-        .write_all(input.as_bytes())
-        .expect("failed to write input to gzip encoder");
-    encoder.finish().into_result().unwrap()
-}
-
-#[pg_extern]
-fn gzip_bytes(input: &[u8]) -> Vec<u8> {
+fn gzip(input: &[u8]) -> Vec<u8> {
     let mut encoder = Encoder::new(Vec::new()).expect("failed to construct gzip Encoder");
     encoder
         .write_all(input)
@@ -22,8 +14,9 @@ fn gzip_bytes(input: &[u8]) -> Vec<u8> {
     encoder.finish().into_result().unwrap()
 }
 
+/// gunzip previously gzipped bytes
 #[pg_extern]
-fn gunzip_bytes(mut bytes: &[u8]) -> Vec<u8> {
+fn gunzip(mut bytes: &[u8]) -> Vec<u8> {
     let mut decoder = Decoder::new(&mut bytes).expect("failed to construct gzip Decoder");
     let mut buf = Vec::new();
     decoder
@@ -32,14 +25,10 @@ fn gunzip_bytes(mut bytes: &[u8]) -> Vec<u8> {
     buf
 }
 
+/// gunzip previously gzipped bytes as a String
 #[pg_extern]
-fn gunzip_text(mut bytes: &[u8]) -> String {
-    let mut decoder = Decoder::new(&mut bytes).expect("failed to construct gzip Decoder");
-    let mut buf = Vec::new();
-    decoder
-        .read_to_end(&mut buf)
-        .expect("failed to decode gzip data");
-    String::from_utf8(buf).expect("decoded text is not valid utf8")
+fn gunzip_as_text(bytes: &[u8]) -> String {
+    String::from_utf8(gunzip(bytes)).expect("decompressed text is not valid utf8")
 }
 
 #[cfg(any(test, feature = "pg_test"))]
@@ -48,14 +37,14 @@ mod tests {
 
     #[pg_test]
     fn test_gzip_text() {
-        let result = Spi::get_one::<String>("SELECT gunzip_text(gzip_text('hi there'));")
+        let result = Spi::get_one::<String>("SELECT gunzip_as_text(gzip('hi there'));")
             .expect("SPI result was null");
         assert_eq!(result, "hi there");
     }
 
     #[pg_test]
     fn test_gzip_bytes() {
-        let result = Spi::get_one::<&[u8]>("SELECT gunzip_bytes(gzip_bytes('hi there'::bytea));")
+        let result = Spi::get_one::<&[u8]>("SELECT gunzip(gzip('hi there'::bytea));")
             .expect("SPI result was null");
         assert_eq!(result, b"hi there");
     }
