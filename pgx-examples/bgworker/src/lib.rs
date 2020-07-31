abd8f68200401baa3d27b50f0a0dfa6a8d0f6ddf
Eric B. Ridge <eebbrr@gmail.com>
2020-07-31 11:37:06 -0600

    cleanup bgworker example

--- pgx-examples/bgworker/src/lib.rs
+++ pgx-examples/bgworker/src/lib.rs
@@ -7,19 +7,20 @@ pg_module_magic!();
 #[allow(non_snake_case)]
 #[pg_guard]
 pub extern "C" fn _PG_init() {
-    BackgroundWorkerBuilder::new("BGWorker demo")
-        .set_function("my_bgw_init")
-        .set_library("libbgworkerdemo")
+    BackgroundWorkerBuilder::new("Background Worker Example")
+        .set_function("background_worker_main")
+        .set_library("bgworker")
         .enable_spi_access()
         .load();
 }
 
-#[allow(non_snake_case)]
 #[pg_guard]
-pub extern "C" fn my_bgw_init() {
-    // These could be hidden with a proc macro on the function. If so you'd need to use the
-    // extra field of the bgworker to pass the user/database settings. Maybe not worth it
+pub extern "C" fn background_worker_main() {
+    // these are the signals we want to receive.  If we don't attach the SIGTERM handler, then
+    // we'll never be able to exit via an external notification
     BackgroundWorker::attach_signal_handlers(SignalWakeFlags::SIGHUP | SignalWakeFlags::SIGTERM);
+
+    // we want to be able to use SPI against the specified table (postgres), as the user postgres
     BackgroundWorker::connect_worker_to_spi(Some("postgres"), Some("postgres"));
 
     log!(
@@ -27,10 +28,13 @@ pub extern "C" fn my_bgw_init() {
         BackgroundWorker::get_name()
     );
 
+    // wake up every 10s or if we received a SIGTERM
     while BackgroundWorker::wait_latch(Some(Duration::from_secs(10))) {
         if BackgroundWorker::sighup_received() {
-            //Do sighuppy stuff
+            // on SIGHUP, you might want to reload some external configuration or something
         }
+
+        // within a transaction, execute an SQL statement, and log its results
         BackgroundWorker::transaction(|| {
             Spi::execute(|client| {
                 let tuple_table = client.select(
@@ -39,9 +43,10 @@ pub extern "C" fn my_bgw_init() {
                     None,
                 );
                 tuple_table.for_each(|tuple| {
-                    tuple.get_datum::<String>(1).unwrap();
-                    tuple.get_datum::<i32>(2).unwrap();
-                    tuple.get_datum::<String>(3).unwrap();
+                    let a = tuple.get_datum::<String>(1).unwrap();
+                    let b = tuple.get_datum::<i32>(2).unwrap();
+                    let c = tuple.get_datum::<String>(3).unwrap();
+                    log!("from bgworker: ({}, {}, {})", a, b, c);
                 });
             });
         });
