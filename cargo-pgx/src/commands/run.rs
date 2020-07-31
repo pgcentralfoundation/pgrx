d717e5fb4ba4964e0dd8838bac1e567d2ba5bc89
Eric B. Ridge <eebbrr@gmail.com>
2020-07-31 12:57:10 -0600

    use https://docs.rs/exec/0.3.1/exec/ when we span psql via `cargo pgx run` so that signals like ^C are properly passed through to psql

--- cargo-pgx/src/commands/run.rs
+++ cargo-pgx/src/commands/run.rs
@@ -1,13 +1,11 @@
 // Copyright 2020 ZomboDB, LLC <zombodb@gmail.com>. All rights reserved. Use of this source code is
 // governed by the MIT license that can be found in the LICENSE file.
 
-
 use crate::commands::install::install_extension;
 use crate::commands::start::start_postgres;
 use crate::commands::stop::stop_postgres;
 use colored::Colorize;
-use pgx_utils::{createdb, get_pg_config, get_psql_path, handle_result, BASE_POSTGRES_PORT_NO};
-use std::process::Stdio;
+use pgx_utils::{createdb, get_pg_config, get_psql_path, BASE_POSTGRES_PORT_NO};
 
 pub(crate) fn run_psql(major_version: u16, dbname: &str) {
     let pg_config = get_pg_config(major_version);
@@ -39,19 +37,14 @@ pub(crate) fn run_psql(major_version: u16, dbname: &str) {
 }
 
 fn exec_psql(major_version: u16, dbname: &str) {
-    let mut command = std::process::Command::new(get_psql_path(major_version));
+    let mut command = exec::Command::new(get_psql_path(major_version));
     command
         .arg("-h")
         .arg("localhost")
         .arg("-p")
         .arg((BASE_POSTGRES_PORT_NO + major_version).to_string())
-        .arg(dbname)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .stdin(Stdio::inherit());
+        .arg(dbname);
 
-    let status = handle_result!("problem starting psql", command.status());
-
-    // as soon as psql exists, exit our process and return its status code
-    std::process::exit(status.code().expect("no return code from psql"))
+    // we'll never return from here as we've now become psql
+    panic!("{}", command.exec());
 }
