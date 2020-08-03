dc4773ab8bacecbfbea13beaf29093b37bc7503d
Eric B. Ridge <eebbrr@gmail.com>
2020-08-03 11:54:04 -0600

    longer sleeps for publish.sh to avoid errors.  nogha

--- publish.sh
+++ publish.sh
@@ -3,10 +3,10 @@
 DIR=`pwd`
 set -x
 
-cd $DIR/pgx-utils && cargo publish && sleep 10
-cd $DIR/pgx-macros && cargo publish && sleep 10
-cd $DIR/pgx-pg-sys && cargo publish --no-verify && sleep 10
-cd $DIR/pgx && cargo publish --no-verify && sleep 10
-cd $DIR/pgx-tests && cargo publish --no-verify && sleep 10
-cd $DIR/cargo-pgx && cargo publish && sleep 10
+cd $DIR/pgx-utils && cargo publish && sleep 30
+cd $DIR/pgx-macros && cargo publish && sleep 30
+cd $DIR/pgx-pg-sys && cargo publish --no-verify && sleep 30
+cd $DIR/pgx && cargo publish --no-verify && sleep 30
+cd $DIR/pgx-tests && cargo publish --no-verify && sleep 30
+cd $DIR/cargo-pgx && cargo publish
 
