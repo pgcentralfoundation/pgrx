e9671a858e89b9b186f3112e6f7490c035a1af0f
Ethan Pailes <ethan@pailes.org>
2020-09-07 14:24:02 -0400

    switch to gating behind env var

--- pgx-pg-sys/build.rs
+++ pgx-pg-sys/build.rs
@@ -47,9 +47,9 @@ impl bindgen::callbacks::ParseCallbacks for IgnoredMacros {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    // dump parts of the environment
-    for (k, v) in std::env::vars() {
-        if k.starts_with("CARGO") {
+    // dump the environment for debugging if asked
+    if std::env::var("PGX_BUILD_VERBOSE").unwrap_or("false".to_string()) == "true" {
+        for (k, v) in std::env::vars() {
             eprintln!("{}={}", k, v);
         }
     }
