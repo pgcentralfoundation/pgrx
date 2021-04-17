c46fd771cfa241bc125c783f29df4d1f15874a6f
Ana Hobden <operator@hoverbear.org>
2021-04-16 17:12:30 -0700

    feat: add Nix flake
    Introduce a Nix flake, a [recently introduced
    concept](https://www.tweag.io/blog/2020-05-25-flakes/) in the Nix
    community.
    
    This will allow our users and developers to optionally use Nix to work
    with `pgx`. This commit is a minimal step, and does not include
    documentation or usage guidelines, as it is not officially supported for
    users yet.
    
    * Introduce a `cargo-pgx` derivation for Nix users.
    
      Users can run `cargo-pgx` ad-hoc:
    
      ```bash
      nix run .#cargo-pgx -- pgx init
      ```
    
      Or they can develop `cargo-pgx` itself:
    
      ```bash
      nix develop .#cargo-pgx
      ```
    * Add a flake file that imports the `cargo-pgx` derivation.
    * Set `cargo-pgx` as the default run/build target:
    
      ```bash
      nix run .
      nix build .
      ```
    * Define a development shell for building `pgx` based extensions.
    
      ```bash
      nix develop .
      ```
    
    * Define checks for formatting (using `rustfmt` and `nixpkgs-fmt`):
    
      ```bash
      nix flake check
      ```
    
    * Add an `.envrc` with `use flakes` for
    [`nix-direnv`](https://github.com/nix-community/nix-direnv) users.
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Postgres extensions in Rust.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      defaultPackage = forAllSystems (system: (import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      }).cargo-pgx);
+
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          };
+        in
+        {
+          inherit (pkgs) cargo-pgx;
+        });
+
+      overlay = final: prev: {
+        cargo-pgx = final.callPackage ./cargo-pgx { };
+      };
+
+      devShell = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          };
+        in
+        pkgs.mkShell {
+          inputsFrom = with pkgs; [
+            postgresql_10
+            postgresql_11
+            postgresql_12
+            postgresql_13
+          ];
+          buildInputs = with pkgs; [
+            rustfmt
+            nixpkgs-fmt
+            cargo-pgx
+          ];
+          LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib";
+        });
+
+      checks = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          };
+        in
+        {
+          format = pkgs.runCommand "check-format"
+            {
+              buildInputs = with pkgs; [ rustfmt cargo ];
+            } ''
+            ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
+            ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
+            touch $out # it worked!
+          '';
+        });
+    };
+}
