48abae2b059fe86c1f4142515a4ba3986fbf1fe5
Ana Hobden <operator@hoverbear.org>
2021-05-10 19:04:35 -0700

    feat: add flake template
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

---
+++ nix/templates/default/src/lib.rs
@@ -0,0 +1,45 @@
+use pgx::*;
+
+pg_module_magic!();
+
+#[pg_extern]
+fn hello() -> &'static str {
+    "Hello"
+}
+
+#[cfg(any(test, feature = "pg_test"))]
+mod tests {
+    use pgx::*;
+
+    #[pg_test]
+    fn test_hello() {
+        assert_eq!("Hello", crate::hello());
+    }
+}
+
+#[cfg(test)]
+pub mod pg_test {
+    use once_cell::sync::Lazy;
+    use pgx_utils::pg_config::Pgx;
+    use tempdir::TempDir;
+
+    static WORK_DIR: Lazy<String> = Lazy::new(|| {
+        let work_dir = TempDir::new("plrust-tests").expect("Couldn't create tempdir");
+        format!("plrust.work_dir='{}'", work_dir.path().display())
+    });
+    static PG_CONFIG: Lazy<String> = Lazy::new(|| {
+        let pgx_config = Pgx::from_config().unwrap();
+        let version = format!("pg{}", pgx_pg_sys::get_pg_major_version_num());
+        let pg_config = pgx_config.get(&version).unwrap();
+        let path = pg_config.path().unwrap();
+        format!("plrust.pg_config='{}'", path.as_path().display())
+    });
+
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        vec![&*WORK_DIR, &*PG_CONFIG]
+    }
+}
