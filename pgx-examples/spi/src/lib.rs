7ca8f52ba7717307c51530d6b12d6adc46e6dad3
Ana Hobden <operator@hoverbear.org>
2021-07-23 14:30:53 -0700

    wip: fixups
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- pgx-examples/spi/src/lib.rs
+++ pgx-examples/spi/src/lib.rs
@@ -35,7 +35,7 @@ fn spi_return_query(
     Spi::connect(|client| {
         client
             .select(query, None, None)
-            .map(|row| (row["oid"].value(), row[2].value()))
+            .map(|row| (row[" oid"].value(), row[2].value()))
             .for_each(|tuple| results.push(tuple));
         Ok(Some(()))
     });
