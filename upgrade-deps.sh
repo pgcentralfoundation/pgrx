aed7bee01c9a196524ded8725cff33867999280c
Ana Hobden <operator@hoverbear.org>
2022-02-01 11:24:38 -0800

    Upgrade deps
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- upgrade-deps.sh
+++ upgrade-deps.sh
@@ -5,3 +5,13 @@ cargo update
 cargo upgrade --workspace
 cargo generate-lockfile
 
+# examples are their own independent crates, so we have to do them individually.
+for folder in pgx-examples/*; do
+    if [ -d "$folder" ]; then
+        cd $folder
+        cargo update
+        cargo upgrade
+        cargo generate-lockfile
+        cd -
+    fi
+done
