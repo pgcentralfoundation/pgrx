adee4a01c52ae9e2b48b17b2b95d2e5d33b74768
Ana Hobden <operator@hoverbear.org>
2022-02-09 10:04:23 -0800

    Rework release script
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- update-versions.sh
+++ update-versions.sh
@@ -3,22 +3,42 @@
 # requires: https://github.com/sunng87/cargo-release
 
 if [ "x$1" == "x" ]; then
-	echo "usage:  ./update-verions.sh <VERSION>"
-	exit 1
+    echo "usage:  ./update-verions.sh <VERSION>"
+    exit 1
 fi
 
-set -x
+set -ex
 
 HEAD=$(git rev-parse HEAD)
 VERSION=$1
 
-cargo release --workspace --no-publish --no-push --no-tag --no-dev-version ${VERSION} || exit 1
-git reset --soft ${HEAD} || exit 1 
-git reset HEAD || exit 1
-sed -i '' -e "s/^pgx = .*$/pgx = \"${VERSION}\"/" ./cargo-pgx/src/templates/cargo_toml || exit 1
-sed -i '' -e "s/^pgx-macros = .*$/pgx-macros = \"${VERSION}\"/" ./cargo-pgx/src/templates/cargo_toml || exit 1
-sed -i '' -e "s/^pgx-tests = .*$/pgx-tests = \"${VERSION}\"/" ./cargo-pgx/src/templates/cargo_toml || exit 1
+CARGO_TOMLS_TO_SED=(
+    ./cargo-pgx/src/templates/cargo_toml
+    ./nix/templates/default/Cargo.toml
+)
 
-sed -i '' -e "s/^pgx = .*$/pgx = \"${VERSION}\"/" ./nix/templates/default/Cargo.toml || exit 1
-sed -i '' -e "s/^pgx-macros = .*$/pgx-macros = \"${VERSION}\"/" ./nix/templates/default/Cargo.toml || exit 1
-sed -i '' -e "s/^pgx-tests = .*$/pgx-tests = \"${VERSION}\"/" ./nix/templates/default/Cargo.toml || exit 1
+CRATES_TO_UPDATE=(
+    ./pgx/Cargo.toml
+    ./pgx-utils/Cargo.toml
+    ./pgx-macros/Cargo.toml
+    ./pgx-tests/Cargo.toml
+    ./cargo-pgx/Cargo.toml
+    ./pgx-pg-sys/Cargo.toml
+    ./pgx-examples/*/Cargo.toml
+)
+
+DEPENDENCIES_TO_UPDATE=(
+    "pgx"
+    "pgx-tests"
+    "pgx-macros"
+    "pgx-pgx-sys"
+    "pgx-utils"
+)
+
+cargo release --workspace --no-publish --no-tag --no-push --no-dev-version --execute 0.3.0
+
+for cargo_toml in ${CARGO_TOMLS_TO_SED[@]}; do
+    for dependency in ${DEPENDENCIES_TO_UPDATE[@]}; do
+        sed -i'' -e "s/^${dependency} = .*$/${dependency} = \"${VERSION}\"/" ${cargo_toml}
+    done
+done
