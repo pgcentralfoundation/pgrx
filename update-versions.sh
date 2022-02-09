1dcb1e25dccf74577e981de589086bbde65b4df5
Ana Hobden <operator@hoverbear.org>
2022-02-09 11:26:42 -0800

    Update versions rework (#429)
    * Update update-versions.sh script for new cargo-release
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * Update update-versions.sh with nix template
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * Rework release script
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * Update-update-versions-script
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>
    
    * Add missing newline
    
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- update-versions.sh
+++ update-versions.sh
@@ -3,21 +3,37 @@
 # requires: https://github.com/sunng87/cargo-release
 
 if [ "x$1" == "x" ]; then
-	echo "usage:  ./update-verions.sh <VERSION>"
-	exit 1
+    echo "usage:  ./update-verions.sh <VERSION>"
+    exit 1
 fi
 
-set -x
+set -ex
 
 HEAD=$(git rev-parse HEAD)
 VERSION=$1
 
-cargo release --workspace --skip-publish --skip-push --skip-tag --no-dev-version ${VERSION} || exit 1
-git reset --soft ${HEAD} || exit 1 
-git reset HEAD || exit 1
-sed -i '' -e "s/^pgx = .*$/pgx = \"${VERSION}\"/" ./cargo-pgx/src/templates/cargo_toml || exit 1
-sed -i '' -e "s/^pgx-macros = .*$/pgx-macros = \"${VERSION}\"/" ./cargo-pgx/src/templates/cargo_toml || exit 1
-sed -i '' -e "s/^pgx-tests = .*$/pgx-tests = \"${VERSION}\"/" ./cargo-pgx/src/templates/cargo_toml || exit 1
-sed -i '' -e "s/version: .*$/version: \"${VERSION}\"/" ./cargo-pgx/src/cli.yml || exit 1
+CARGO_TOMLS_TO_SED=(
+    ./cargo-pgx/src/templates/cargo_toml
+    ./nix/templates/default/Cargo.toml
+    ./pgx/Cargo.toml
+    ./pgx-utils/Cargo.toml
+    ./pgx-macros/Cargo.toml
+    ./pgx-tests/Cargo.toml
+    ./cargo-pgx/Cargo.toml
+    ./pgx-pg-sys/Cargo.toml
+    ./pgx-examples/*/Cargo.toml
+)
 
+DEPENDENCIES_TO_UPDATE=(
+    "pgx"
+    "pgx-tests"
+    "pgx-macros"
+    "pgx-pgx-sys"
+    "pgx-utils"
+)
 
+for cargo_toml in ${CARGO_TOMLS_TO_SED[@]}; do
+    for dependency in ${DEPENDENCIES_TO_UPDATE[@]}; do
+        sed -i'' -E "s/(^${dependency}.*\")[0-9]+\.[0-9]+\.[0-9]+(\".*$)/\1${VERSION}\2/" ${cargo_toml}
+    done
+done
