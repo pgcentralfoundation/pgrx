3f226c361b9abf35b800285ddc942623166a58e4
Ana Hobden <operator@hoverbear.org>
2022-02-16 12:35:54 -0800

    Fix test name
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- pgx-tests/src/tests/pg_extern_tests.rs
+++ pgx-tests/src/tests/pg_extern_tests.rs
@@ -34,7 +34,7 @@ mod tests {
     
     // Ensures `@FUNCTION_NAME@` is handled.
     #[pg_extern(sql = r#"
-        CREATE OR REPLACE FUNCTION tests."test_overridden_sql_with_fn_name"() RETURNS void
+        CREATE OR REPLACE FUNCTION tests."overridden_sql_with_fn_name"() RETURNS void
         STRICT
         LANGUAGE c /* Rust */
         AS 'MODULE_PATHNAME', '@FUNCTION_NAME@';
@@ -46,7 +46,7 @@ mod tests {
     #[pg_test]
     fn test_overridden_sql_with_fn_name() {
         let result = Spi::get_one::<bool>(
-            "SELECT tests.overridden_sql_with_fn_name()",
+            r#"SELECT tests."overridden_sql_with_fn_name"()"#,
         )
         .expect("failed to get SPI result");
         assert!(result)
