e34993bb35f547fa5dd7f391209129904e7f755a
Eric B. Ridge <eebbrr@gmail.com>
2022-03-28 09:17:47 -0400

    update headers with better wording

--- pgx/src/datum/into.rs
+++ pgx/src/datum/into.rs
@@ -1,6 +1,7 @@
-// Copyright 2019-2022 ZomboDB, LLC and Technology Concepts & Design, Inc.
-// <support@tcdi.com>. All rights reserved.  Use of this source code is governed
-// by the MIT license that can be found in the LICENSE file.
+// Portions Copyright 2019-2021 ZomboDB, LLC.  Portions Copyright 2021-2022
+// Technology Concepts & Design, Inc. <support@tcdi.com>.  All rights reserved.  Use of this
+// source code is governed by the MIT license that can be found in the LICENSE
+// file.
 
 //! for converting primitive types into Datums
 //!
