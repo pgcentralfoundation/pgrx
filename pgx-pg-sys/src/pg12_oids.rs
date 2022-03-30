7be30b1e1b4027a801a1f6806848005539dc3759
Eric Ridge <eebbrr@gmail.com>
2022-03-30 14:10:07 -0400

    Prepare 0.4.2 (#522)
    * upgrade dependencies
    
    * bump version to v0.4.2

--- pgx-pg-sys/src/pg12_oids.rs
+++ pgx-pg-sys/src/pg12_oids.rs
@@ -1,11 +1,3 @@
-/*
-Portions Copyright 2019-2021 ZomboDB, LLC.
-Portions Copyright 2021-2022 Technology Concepts & Design, Inc. <support@tcdi.com>
-
-All rights reserved.
-
-Use of this source code is governed by the MIT license that can be found in the LICENSE file.
-*/
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Ord, PartialOrd, Debug)]
 pub enum PgBuiltInOids {
     HEAP_TABLE_AM_HANDLER_OID = crate::HEAP_TABLE_AM_HANDLER_OID as isize,
