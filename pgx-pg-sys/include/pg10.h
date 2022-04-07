4cc6a134e961b8d07ff6be5eef296762d5231345
Nikhil <nikhil@timescale.com>
2022-04-07 20:43:27 +0530

    Add support for pg_authid (#527)
    Add entries for pg_authid catalog table in all versions

--- pgx-pg-sys/include/pg10.h
+++ pgx-pg-sys/include/pg10.h
@@ -31,6 +31,7 @@ Use of this source code is governed by the MIT license that can be found in the 
 #include "catalog/index.h"
 #include "catalog/namespace.h"
 #include "catalog/objectaddress.h"
+#include "catalog/pg_authid.h"
 #include "catalog/pg_class.h"
 #include "catalog/pg_enum.h"
 #include "catalog/pg_operator.h"
