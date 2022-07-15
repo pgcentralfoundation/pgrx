8d8f8cba772ee1f68d352483e53860cb029eb04b
Luc Perkins <lucperkins@gmail.com>
2022-07-15 20:50:35 +0200

    Fix flake warning about overlays

--- cargo-pgx/default.nix
+++ cargo-pgx/default.nix
@@ -1,24 +1,22 @@
-{ buildRustPackage
+{ gitignoreSource
 , lib
 , pkg-config
 , openssl
-, rustToolchain
 , libiconv
 , llvmPackages
-, gitignoreSource
+, rustPlatform
 , release ? true
 }:
 
 let
   cargoToml = (builtins.fromTOML (builtins.readFile ./Cargo.toml));
-in buildRustPackage {
+in rustPlatform.buildRustPackage {
   inherit release;
   inherit (cargoToml.package) name version;
   cargoSha256 = "sha256-bxzBYrQGA3PmwGh5B2qUaOM9oif9hwk2OR5ZcfDkltY=";
   src = gitignoreSource ../.;
 
   nativeBuildInputs = [
-    rustToolchain
     pkg-config
   ];
   buildInputs = [
--- flake.nix
+++ flake.nix
@@ -15,36 +15,31 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, gitignore, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        inherit (pkgs) callPackage;
-        inherit (pkgs.rustPlatform) buildRustPackage;
-        inherit (gitignore.lib) gitignoreSource;
-
+    let
+      # Helper function for producing a package-specific nixpkgs
+      nixpkgsWithOverlays = { nixpkgs, system, extraOverlays ? [ ] }: (import nixpkgs {
+        inherit system;
         overlays = [
+          self.overlays.default
           (import rust-overlay)
-        ];
+          (self: super: { inherit (self.rust-bin.stable.latest) rustc cargo rustdoc rust-std; })
+        ] ++ extraOverlays;
+      });
 
-        pkgs = import nixpkgs { inherit overlays system; };
-        
-        rustToolchain = pkgs.rust-bin.stable.latest.default;
-
-        cargo-pgx = callPackage ./cargo-pgx {
-          inherit buildRustPackage gitignoreSource rustToolchain;
-        };
-
-        cargo-pgx_debug = callPackage ./cargo-pgx {
-          release = false;
-          inherit buildRustPackage gitignoreSource rustToolchain;
-        };
-      in {
-        packages = {
+      # Inheritance helpers
+      inherit (gitignore.lib) gitignoreSource;
+    in flake-utils.lib.eachDefaultSystem (system:
+      {
+        packages = let
+          pkgs = nixpkgsWithOverlays { inherit nixpkgs system; };
+        in rec {
           default = cargo-pgx;
-
-          inherit cargo-pgx cargo-pgx_debug;
+          inherit (pkgs) cargo-pgx;
         };
 
-        devShells.default = pkgs.mkShell {
+        devShells.default = let
+          pkgs = nixpkgsWithOverlays { inherit nixpkgs system; };
+        in pkgs.mkShell {
           inputsFrom = with pkgs; [
             postgresql_10
             postgresql_11
@@ -52,17 +47,18 @@
             postgresql_13
             postgresql_14
           ];
-          buildInputs = with pkgs; [
+           buildInputs = with pkgs; [
             rustfmt
             nixpkgs-fmt
             cargo-pgx
-            rust-bin.stable.latest.minimal
-            rust-bin.stable.latest.rustfmt
-            rust-bin.stable.latest.clippy
             postgresql
             libiconv
             pkg-config
-          ];
+          ] ++ (with pkgs.rust-bin.stable.latest; [
+            clippy
+            minimal
+            rustfmt
+          ]);
           LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           PGX_PG_SYS_SKIP_BINDING_REWRITE = "1";
           BINDGEN_EXTRA_CLANG_ARGS = [
@@ -72,19 +68,31 @@
           ] else [ ]);
         };
 
-        checks = {
+        checks = let
+          pkgs = nixpkgsWithOverlays { inherit system nixpkgs; };
+        in {
           format = pkgs.runCommand "check-format"
             {
-              buildInputs = with pkgs; [ cargo rustfmt ];
+              buildInputs = with pkgs; [ rustfmt cargo ];
             } ''
             ${pkgs.rustfmt}/bin/cargo-fmt fmt --manifest-path ${./.}/Cargo.toml -- --check
             ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
             touch $out # it worked!
           '';
-          pkgs-cargo-pgx = cargo-pgx_debug.out;
+          pkgs-cargo-pgx = pkgs.cargo-pgx_debug.out;
         };
       }
     ) // {
+      overlays.default = final: prev: {
+        cargo-pgx = final.callPackage ./cargo-pgx {
+          inherit gitignoreSource;
+        };
+        cargo-pgx_debug = final.callPackage ./cargo-pgx {
+          inherit gitignoreSource;
+          release = false;
+        };
+      };
+
       templates = {
         default = {
           path = ./nix/templates/default;
