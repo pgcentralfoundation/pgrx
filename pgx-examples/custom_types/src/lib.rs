c1d9fc9c6f9f849039dc86a0549c9717fad63d5d
Jubilee Young <workingjubilee@gmail.com>
2022-09-22 10:59:23 -0700

    Cleanup an unused import

--- pgx-examples/custom_types/src/lib.rs
+++ pgx-examples/custom_types/src/lib.rs
@@ -7,8 +7,6 @@ All rights reserved.
 Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 */
 
-use pgx::prelude::*;
-
 mod complex;
 mod fixed_size;
 mod generic_enum;
