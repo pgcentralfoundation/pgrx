ecfb15b93a75d48037189f472eb6cea4127c8f03
Brady Bonnette <b_bonnette@tcdi.com>
2022-09-27 11:13:28 -0400

    update-versions.sh uses cargo-edit to update main project files

--- update-versions.sh
+++ update-versions.sh
@@ -10,6 +10,7 @@
 
 # requires:
 # * ripgrep
+# * Cargo extension 'cargo-edit'
 
 if [ "x$1" == "x" ]; then
     echo "usage:  ./update-verions.sh <VERSION>"
@@ -18,62 +19,89 @@ fi
 
 set -ex
 
+if ! which rg &> /dev/null; then
+    echo "Command `rg` (ripgrep) was not found. Please install it and try again."
+    exit 1
+fi
+
+if ! cargo set-version --help &> /dev/null; then
+    echo "Cargo extension `cargo-edit` is not installed. Please install it by running: cargo install cargo-edit"
+    exit 1
+fi
+
 HEAD=$(git rev-parse HEAD)
 VERSION=$1
 
-# ordered in a topological fashion starting from the workspace root Cargo.toml
-# this isn't always necessary, but it's nice to use a mostly-consistent ordering
-CARGO_TOMLS_TO_BUMP=(
-    ./Cargo.toml
-    ./pgx-pg-config/Cargo.toml
-    ./pgx-utils/Cargo.toml
-    ./cargo-pgx/Cargo.toml
-    ./pgx-macros/Cargo.toml
-    ./pgx-pg-sys/Cargo.toml
-    ./pgx/Cargo.toml
-    ./pgx-tests/Cargo.toml
-)
+# Use `cargo set-version` to update all main project files
+function update_main_files() {
+    local version=$1
 
-CARGO_TOMLS_TO_SED=(
-    ./Cargo.toml
-    ./pgx-pg-config/Cargo.toml
-    ./pgx-utils/Cargo.toml
-    ./cargo-pgx/Cargo.toml
-    ./pgx-macros/Cargo.toml
-    ./pgx-pg-sys/Cargo.toml
-    ./pgx/Cargo.toml
-    ./pgx-tests/Cargo.toml
-    ./pgx-examples/*/Cargo.toml
-    ./cargo-pgx/src/templates/cargo_toml
-    ./nix/templates/default/Cargo.toml
-)
+    EXCLUDE_PACKAGES=()
 
-DEPENDENCIES_TO_UPDATE=(
-    "pgx-pg-config"
-    "pgx-utils"
-    "cargo-pgx"
-    "pgx-macros"
-    "pgx-pgx-sys"
-    "pgx"
-    "pgx-tests"
-)
+    # Add additional packages to ignore by using the following syntax:
+    # EXCLUDE_PACKAGES+=('foo' 'bar' 'baz')
 
-SEMVER_REGEX="(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
+    # Ignore all packages in pgx-examples/
+    for file in $(find pgx-examples/ -type f -name "Cargo.toml"); do
+        EXCLUDE_PACKAGES+=($(cat $file | rg --multiline '\[package\](.*\n)(?:[^\[]*\n)*name\s?=\s?"(?P<name>[-_a-z]*)"(.*\n)*' -r "\${name}"))
+    done
 
-for cargo_toml in ${CARGO_TOMLS_TO_SED[@]}; do
-    for dependency in ${DEPENDENCIES_TO_UPDATE[@]}; do
-        rg --passthru --no-line-number \
-        "(?P<prefix>^${dependency}.*\")(?P<pin>=?)${SEMVER_REGEX}(?P<postfix>\".*$)" \
-        -r "\${prefix}=${VERSION}\${postfix}" \
-        ${cargo_toml} > ${cargo_toml}.tmp || true
+    echo "Excluding the following packages:"
+    for p in "${EXCLUDE_PACKAGES[@]}"; do
+        echo " * $p"
+    done
+    echo ""
+
+    cargo set-version $(echo "${EXCLUDE_PACKAGES[@]/#/--exclude }") --workspace $version
+}
+
+# This is a legacy holdover for updating extra toml files throughout various crates
+function update_extras() {
+    local version=$1
+
+    # ordered in a topological fashion starting from the workspace root Cargo.toml
+    # this isn't always necessary, but it's nice to use a mostly-consistent ordering
+    CARGO_TOMLS_TO_BUMP=(
+        ./Cargo.toml
+    )
+
+    CARGO_TOMLS_TO_SED=(
+        ./Cargo.toml
+        ./pgx-examples/*/Cargo.toml
+        ./cargo-pgx/src/templates/cargo_toml
+        ./nix/templates/default/Cargo.toml
+    )
+
+    DEPENDENCIES_TO_UPDATE=(
+        "pgx-pg-config"
+        "pgx-utils"
+        "cargo-pgx"
+        "pgx-macros"
+        "pgx-pgx-sys"
+        "pgx"
+        "pgx-tests"
+    )
+
+    SEMVER_REGEX="(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
+
+    for cargo_toml in ${CARGO_TOMLS_TO_SED[@]}; do
+        for dependency in ${DEPENDENCIES_TO_UPDATE[@]}; do
+            rg --passthru --no-line-number \
+            "(?P<prefix>^${dependency}.*\")(?P<pin>=?)${SEMVER_REGEX}(?P<postfix>\".*$)" \
+            -r "\${prefix}=${VERSION}\${postfix}" \
+            ${cargo_toml} > ${cargo_toml}.tmp || true
+            mv ${cargo_toml}.tmp ${cargo_toml}
+        done
+    done
+
+    for cargo_toml in ${CARGO_TOMLS_TO_BUMP[@]}; do
+        rg --passthru --no-line-number "(?P<prefix>^version = \")${SEMVER_REGEX}(?P<postfix>\"$)" -r "\${prefix}${VERSION}\${postfix}" ${cargo_toml} > ${cargo_toml}.tmp || true
         mv ${cargo_toml}.tmp ${cargo_toml}
     done
-done
+}
 
-for cargo_toml in ${CARGO_TOMLS_TO_BUMP[@]}; do
-    rg --passthru --no-line-number "(?P<prefix>^version = \")${SEMVER_REGEX}(?P<postfix>\"$)" -r "\${prefix}${VERSION}\${postfix}" ${cargo_toml} > ${cargo_toml}.tmp || true
-    mv ${cargo_toml}.tmp ${cargo_toml}
-done
+update_main_files $VERSION
+update_extras $VERSION
 
 cargo generate-lockfile
 
