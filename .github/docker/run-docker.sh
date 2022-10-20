aa8b7a4ee39ab81a0f885c6be99417a96f832c44
Brady Bonnette <b_bonnette@tcdi.com>
2022-10-20 17:58:16 -0400

    Lets try all of them

--- .github/docker/run-docker.sh
+++ .github/docker/run-docker.sh
@@ -1,13 +1,15 @@
 #! /usr/bin/env bash
 
-PG_VER=$1
+PG_MAJOR_VER=$1
 DOCKERFILE_ID=$2
 
-echo "Building docker container for PGX using Postgres version $PG_VER in container $DOCKERFILE_ID"
+echo "Building docker container for PGX using Postgres version $PG_MAJOR_VER in container $DOCKERFILE_ID"
 
-docker build -t pgx -f ".github/docker/Dockerfile.$DOCKERFILE_ID" .
+docker build \
+  --build-arg PG_MAJOR_VER=$PG_MAJOR_VER \
+  -t pgx -f ".github/docker/Dockerfile.$DOCKERFILE_ID" .
 
-echo "Running PGX test suite using Postgres version $PG_VER in container $DOCKERFILE_ID"
+echo "Running PGX test suite using Postgres version $PG_MAJOR_VER in container $DOCKERFILE_ID"
 
 docker run pgx
 # docker run \
