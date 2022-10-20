5df1e8a0d2bc6f14ffa780f741efa86882f9cd09
Brady Bonnette <b_bonnette@tcdi.com>
2022-10-20 15:47:57 -0400

    Trying something

---
+++ .github/docker/run-docker.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+
+PG_VER=$1
+DOCKERFILE_ID=$2
+
+echo "Building docker container for PGX using Postgres version $PG_VER in container $DOCKERFILE_ID"
+
+docker build -t pgx -f ".github/docker/Dockerfile$DOCKERFILE_ID" .
+
+echo "Running PGX test suite using Postgres version $PG_VER in container $DOCKERFILE_ID"
+
+docker run \
+  --rm \
+  --volume "$(pwd)":/checkout:rw \
+  --init \
+  --workdir /checkout \
+  --privileged \
+  pgx
+#   target=$(echo "${1}" | sed 's/-emulated//')
+#     echo "Building docker container for TARGET=${1}"
+#     docker build -t stdarch -f "ci/docker/${1}/Dockerfile" ci/
+#     mkdir -p target c_programs rust_programs
+#     echo "Running docker"
+#     # shellcheck disable=SC2016
+#     docker run \
+#       --rm \
+#       --user "$(id -u)":"$(id -g)" \
+#       --env CARGO_HOME=/cargo \
+#       --env CARGO_TARGET_DIR=/checkout/target \
+#       --env TARGET="${target}" \
+#       --env STDARCH_TEST_EVERYTHING \
+#       --env STDARCH_ASSERT_INSTR_IGNORE \
+#       --env STDARCH_DISABLE_ASSERT_INSTR \
+#       --env NOSTD \
+#       --env NORUN \
+#       --env RUSTFLAGS \
+#       --env STDARCH_TEST_NORUN \
+#       --volume "${HOME}/.cargo":/cargo \
+#       --volume "$(rustc --print sysroot)":/rust:ro \
+#       --volume "$(pwd)":/checkout:ro \
+#       --volume "$(pwd)"/target:/checkout/target \
+#       --volume "$(pwd)"/c_programs:/checkout/c_programs \
+#       --volume "$(pwd)"/rust_programs:/checkout/rust_programs \
+#       --init \
+#       --workdir /checkout \
+#       --privileged \
+#       stdarch \
+#       sh -c "HOME=/tmp PATH=\$PATH:/rust/bin exec ci/run.sh ${1}"
