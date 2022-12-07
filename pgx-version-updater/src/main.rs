6d66831b37abb3bc922e3013febf27689b9c88e5
Jubilee Young <workingjubilee@gmail.com>
2022-12-07 15:20:45 -0800

    version-updater: deny clippy::needless_borrow

--- pgx-version-updater/src/main.rs
+++ pgx-version-updater/src/main.rs
@@ -1,3 +1,4 @@
+#![deny(clippy::needless_borrow)] // unnecessary borrows can impair inference
 use clap::{Args, Parser, Subcommand};
 use owo_colors::OwoColorize;
 use std::collections::HashSet;
@@ -90,7 +91,7 @@ fn query_toml(query_args: &QueryCargoVersionArgs) {
     // the root of a PGX checkout directory
     let filepath = match &query_args.file_path {
         Some(path) => {
-            fullpath(&path).expect(format!("Could not get full path for file: {}", path).as_str())
+            fullpath(path).expect(format!("Could not get full path for file: {}", path).as_str())
         }
         None => {
             let mut current_dir = env::current_dir().expect("Could not get current_dir!");
@@ -135,7 +136,7 @@ fn update_files(args: &UpdateFilesArgs) {
     let mut exclude_version_files = HashSet::new();
     for file in &args.exclude_from_version_change {
         exclude_version_files.insert(
-            fullpath(&file).expect(format!("Could not get full path for file: {}", file).as_str()),
+            fullpath(file).expect(format!("Could not get full path for file: {}", file).as_str()),
         );
     }
 
@@ -184,7 +185,7 @@ fn update_files(args: &UpdateFilesArgs) {
     // Loop through all files that are included for dependency updates via CLI params
     for file in &args.include_for_dep_updates {
         let filepath =
-            fullpath(&file).expect(format!("Could not get full path for file {}", file).as_str());
+            fullpath(file).expect(format!("Could not get full path for file {}", file).as_str());
 
         let mut output = format!(
             "{} Cargo.toml file at {} for processing",
@@ -261,10 +262,7 @@ fn update_files(args: &UpdateFilesArgs) {
 
         let update_package_version = |item: &mut Item| {
             if let Some(current_version_specifier) = item.as_str() {
-                *item = value(parse_new_version(
-                    current_version_specifier,
-                    &args.update_version.as_str(),
-                ))
+                *item = value(parse_new_version(current_version_specifier, &args.update_version))
             }
         };
 
@@ -447,7 +445,7 @@ fn parse_new_version(current_version_specifier: &str, new_version: &str) -> Stri
 fn extract_package_name<P: AsRef<Path>>(filepath: P) -> Option<String> {
     let filepath = filepath.as_ref();
 
-    let data = fs::read_to_string(&filepath)
+    let data = fs::read_to_string(filepath)
         .expect(format!("Unable to open file at {}", &filepath.display()).as_str());
 
     let doc = data.parse::<Document>().expect(
