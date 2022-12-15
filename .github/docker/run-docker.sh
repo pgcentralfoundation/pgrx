5d2e324cdbd7696ec14fa1065e0043bf71f57311
Brady Bonnette <b_bonnette@tcdi.com>
2022-12-15 14:58:07 -0500

    couple doc cleanups; add everything back to test

--- .github/docker/run-docker.sh
+++ .github/docker/run-docker.sh
@@ -1,5 +1,12 @@
 #! /usr/bin/env bash
 
+# Environment variables:
+#   PG_MAJOR_VER: The major version of Postgres in which to build/run. E.g. 14, 12, 15
+#   DOCKERFILE_ID: The Dockerfile identifier to be built, included in this repo,
+#                  e.g. debian:bullseye or amazon:2
+#   CARGO_LOCKED_OPTION: Set to '--locked' to use "cargo --locked", or set to
+#                        blank '' to use "cargo" without "--locked"
+
 # Examples of running this script in CI (currently Github Actions):
 #   ./.github/docker/run-docker.sh 14 debian:bullseye
 #   ./.github/docker/run-docker.sh 12 fedora:36
