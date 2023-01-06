cc28b62e2948aa1d535d9f592057288c7ac8e70d
Eric B. Ridge <eebbrr@gmail.com>
2023-01-06 16:17:23 -0500

    `PgMemoryContexts.unguarded_switch_to()` was deleted, so it doesn't need to be used under the `postgrestd` feature.

--- pgx/src/memcxt.rs
+++ pgx/src/memcxt.rs
@@ -589,7 +589,7 @@ impl PgMemoryContexts {
     pub fn leak_trivial_alloc<T>(&mut self, v: T) -> *mut T {
         #[cfg(feature = "postgrestd")]
         {
-            self.unguarded_switch_to(|_cx| Box::leak(Box::new(v)))
+            Box::leak(Box::new(v))
         }
         #[cfg(not(feature = "postgrestd"))]
         {
