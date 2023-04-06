8941f21c8fae1167d2698d41bd00f4c0bb318941
Thom Chiovoloni <thom@shift.click>
2023-04-06 14:47:06 -0700

    Allow `clippy::no_mangle_with_rust_abi` in the entity graph (#1080)

--- pgx-sql-entity-graph/src/postgres_enum/mod.rs
+++ pgx-sql-entity-graph/src/postgres_enum/mod.rs
@@ -139,6 +139,7 @@ impl ToEntityGraphTokens for PostgresEnum {
 
             #[no_mangle]
             #[doc(hidden)]
+            #[allow(unknown_lints, clippy::no_mangle_with_rust_abi)]
             pub extern "Rust" fn  #sql_graph_entity_fn_name() -> ::pgx::pgx_sql_entity_graph::SqlGraphEntity {
                 extern crate alloc;
                 use alloc::vec::Vec;
