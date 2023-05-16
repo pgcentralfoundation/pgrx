8d0c66075194e6408162805a4deb79e838da3930
Eric B. Ridge <eebbrr@gmail.com>
2023-05-16 15:55:45 -0400

    Within the NUMERIC support code, stop comparing function pointers
    It seems this isn't a thing Rust does predictably and was causing some
    bizarre test failures in a different WIP branch.

--- pgrx/src/datum/numeric_support/convert.rs
+++ pgrx/src/datum/numeric_support/convert.rs
@@ -11,31 +11,57 @@ use crate::{
 pub use super::convert_anynumeric::*;
 pub use super::convert_numeric::*;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub(crate) enum FromPrimitiveFunc {
+    NumericIn,
+    Numeric,
+    Float4Numeric,
+    Float8Numeric,
+    Int2Numeric,
+    Int4Numeric,
+    Int8Numeric,
+}
+
+impl From<FromPrimitiveFunc> for unsafe fn(pg_sys::FunctionCallInfo) -> pg_sys::Datum {
+    fn from(value: FromPrimitiveFunc) -> Self {
+        match value {
+            FromPrimitiveFunc::NumericIn => pg_sys::numeric_in,
+            FromPrimitiveFunc::Numeric => pg_sys::numeric,
+            FromPrimitiveFunc::Float4Numeric => pg_sys::float4_numeric,
+            FromPrimitiveFunc::Float8Numeric => pg_sys::float8_numeric,
+            FromPrimitiveFunc::Int2Numeric => pg_sys::int2_numeric,
+            FromPrimitiveFunc::Int4Numeric => pg_sys::int4_numeric,
+            FromPrimitiveFunc::Int8Numeric => pg_sys::int8_numeric,
+        }
+    }
+}
+
 pub(crate) fn from_primitive_helper<I: IntoDatum, const P: u32, const S: u32>(
     value: I,
-    func: unsafe fn(pg_sys::FunctionCallInfo) -> pg_sys::Datum,
+    func: FromPrimitiveFunc,
 ) -> Result<Numeric<P, S>, Error> {
     let datum = value.into_datum();
     let materialize_numeric_datum = move || unsafe {
-        if func == pg_sys::numeric_in {
+        if func == FromPrimitiveFunc::NumericIn {
             debug_assert_eq!(I::type_oid(), pg_sys::CSTRINGOID);
             direct_function_call(
                 pg_sys::numeric_in,
                 &[datum, pg_sys::InvalidOid.into_datum(), make_typmod(P, S).into_datum()],
             )
-        } else if func == pg_sys::numeric {
+        } else if func == FromPrimitiveFunc::Numeric {
             debug_assert_eq!(I::type_oid(), pg_sys::NUMERICOID);
             direct_function_call(pg_sys::numeric, &[datum, make_typmod(P, S).into_datum()])
         } else {
-            debug_assert!(
-                func == pg_sys::float4_numeric
-                    || func == pg_sys::float8_numeric
-                    || func == pg_sys::int2_numeric
-                    || func == pg_sys::int4_numeric
-                    || func == pg_sys::int8_numeric
-            );
+            debug_assert!(matches!(
+                func,
+                FromPrimitiveFunc::Float4Numeric
+                    | FromPrimitiveFunc::Float8Numeric
+                    | FromPrimitiveFunc::Int2Numeric
+                    | FromPrimitiveFunc::Int4Numeric
+                    | FromPrimitiveFunc::Int8Numeric
+            ));
             // use the user-provided `func` to make a Numeric from some primitive type
-            let numeric_datum = direct_function_call_as_datum(func, &[datum]);
+            let numeric_datum = direct_function_call_as_datum(func.into(), &[datum]);
 
             if P != 0 || S != 0 {
                 // and if it has a precision or a scale, try to coerce it into those constraints
