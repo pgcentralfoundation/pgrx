189bbe01f2602003ab63e10765a612379960349f
Eric Ridge <eebbrr@gmail.com>
2023-06-05 13:30:18 -0400

    `PgVarlena` now knows how to be used by `#[derive(PostgresEq/Ord/Hash)]` (#1159)
    The `PgVarlena` type was lacking some trait impls for doing proper comparisions of the `T` it wraps.  Additionally, the macros that implement the PostgresEq/Ord/Hash operators didn't know about `PgVarlena` and were emitting incorrect Rust wrappers.
    
    Add a new example in `pgrx-examples/operators/src/pgverlena.rs` with a unit test.
    
    As a drive-by, `impl std::fmt::Write for StringInfo` (in `stringinfo.rs`).

--- pgrx-examples/operators/src/lib.rs
+++ pgrx-examples/operators/src/lib.rs
@@ -11,6 +11,7 @@ use pgrx::prelude::*;
 use pgrx::{opname, pg_operator};
 use serde::{Deserialize, Serialize};
 mod derived;
+mod pgvarlena;
 
 pgrx::pg_module_magic!();
 
@@ -23,4 +24,16 @@ pub struct MyType {
 #[opname(=)]
 fn my_eq(left: MyType, right: MyType) -> bool {
     left == right
+}
+
+#[cfg(test)]
+pub mod pg_test {
+    pub fn setup(_options: Vec<&str>) {
+        // perform one-off initialization when the pg_test framework starts
+    }
+
+    pub fn postgresql_conf_options() -> Vec<&'static str> {
+        // return any postgresql.conf settings that are required for your tests
+        vec![]
+    }
 }
