d41755f59b16149d1d319c05737149ebdd84b288
Jubilee <46493976+workingjubilee@users.noreply.github.com>
2023-07-20 15:01:59 -0700

    Stop SpiClient soundness from regressing (#1214)
    * Stop SpiClient soundness from regressing
    
    ...by adding a regression test with trybuild!
    Adds ui tests using trybuild's test runner.
    
    * Bump SCCACHE_MAX_FRAME_LENGTH
    
    * Revert "Disable versioned_custom_libname_so"
    
    This reverts commit 9d530eec008830c8bd42f03a93f1c548b11c79cd.
    
    * Revert "Disable hello_versioned_so test (#1192)"
    
    This reverts commit 8f127982a357bf7c480ae4c6a655dd34717940c6.
    
    * Clean up midway
    
    This adds a "cleanup" midway checkpoint that erases the target directory.
    It's so terribly sad, but this is what is needed for now, I guess.

---
+++ pgrx-tests/tests/ui/escaping-spiclient-1209-prep-stmt.rs
@@ -0,0 +1,13 @@
+use pgrx::prelude::*;
+use std::error::Error;
+
+#[pg_extern]
+fn issue1209_prepared_stmt(q: &str) -> Result<Option<String>, Box<dyn Error>> {
+    use pgrx::spi::Query;
+
+    let prepared = { Spi::connect(|c| c.prepare(q, None))? };
+
+    Ok(Spi::connect(|c| prepared.execute(&c, Some(1), None)?.first().get(1))?)
+}
+
+fn main() {}
