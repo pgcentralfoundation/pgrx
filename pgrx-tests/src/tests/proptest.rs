f732e82fc317ed919cac92a86f66c6dabfc3a5ba
Jubilee Young <workingjubilee@gmail.com>
2023-08-09 07:22:04 -0700

    proptest base

---
+++ pgrx-tests/src/tests/proptest.rs
@@ -0,0 +1,19 @@
+use proptest::strategy::{Strategy, ValueTree};
+
+
+struct ArrayValueTree {}
+
+
+
+struct TimeValueTree {}
+struct TimeWithTimezoneValueTree {}
+struct TimestampValueTree {}
+struct TimestampWithTimezoneValueTree {}
+struct UuidValueTree {}
+struct JsonValueTree {}
+struct NumericValueTree {}
+struct DateValueTree {}
+
+fn create_array_sql_repr() -> ! {
+
+}
