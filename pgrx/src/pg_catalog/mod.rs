b6f8ace0cb0214b15f82df3d3048d094dbeee288
Eric B. Ridge <eebbrr@gmail.com>
2023-09-08 10:21:07 -0400

    pg_proc uses PgList, which requires the cshim feature

--- pgrx/src/pg_catalog/mod.rs
+++ pgrx/src/pg_catalog/mod.rs
@@ -7,4 +7,6 @@
 //LICENSE All rights reserved.
 //LICENSE
 //LICENSE Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#[cfg(feature = "cshim")]
 pub mod pg_proc;
