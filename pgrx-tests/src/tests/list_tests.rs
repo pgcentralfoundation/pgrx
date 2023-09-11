8f0c2a6af18dd0472c612f40d7da667e2e8f5c0e
Jubilee Young <workingjubilee@gmail.com>
2023-09-11 12:39:57 -0700

    Regression test for length bug

---
+++ pgrx-tests/src/tests/list_tests.rs
@@ -0,0 +1,24 @@
+//LICENSE Copyright 2023-2023 PgCentral Foundation, Inc. <contact@pgcentral.org>
+//LICENSE
+//LICENSE All rights reserved.
+//LICENSE
+//LICENSE Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+
+#[cfg(any(test, feature = "pg_test"))]
+#[pgrx::pg_schema]
+mod tests {
+    use crate as pgrx_tests;
+    use pgrx::list::List;
+    use pgrx::prelude::*;
+
+    #[pg_test]
+    fn list_length() {
+        let mut list = List::Nil;
+        for i in 0..1000 {
+            unsafe {
+                assert!(i as usize == list.len());
+                list.unstable_push_in_context(i, pg_sys::CurrentMemoryContext);
+            }
+        }
+    }
+}
