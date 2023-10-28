5fbfd04bc740f14c3cc2b2aed109beff41bf6e95
Jubilee Young <workingjubilee@gmail.com>
2023-10-27 17:08:38 -0700

    Fix deprecation version

--- pgrx-pg-sys/src/cshim.rs
+++ pgrx-pg-sys/src/cshim.rs
@@ -12,7 +12,7 @@ extern "C" {
     pub fn pgrx_list_nth_cell(list: *mut pg_sys::List, nth: i32) -> *mut pg_sys::ListCell;
 
     #[link_name = "pgrx_planner_rt_fetch"]
-    #[deprecated(since = "0.11", note = "use pgrx::pg_sys::planner_rt_fetch")]
+    #[deprecated(since = "0.11.0", note = "use pgrx::pg_sys::planner_rt_fetch")]
     pub fn planner_rt_fetch(
         index: pg_sys::Index,
         root: *mut pg_sys::PlannerInfo,
@@ -33,7 +33,7 @@ extern "C" {
 ///     ((RangeTblEntry *) list_nth(rangetable, (rangetable_index)-1))
 /// ```
 #[inline]
-#[deprecated(since = "0.11", note = "use pgrx::pg_sys::rt_fetch")]
+#[deprecated(since = "0.11.0", note = "use pgrx::pg_sys::rt_fetch")]
 pub unsafe fn rt_fetch(
     index: super::Index,
     range_table: *mut super::List,
