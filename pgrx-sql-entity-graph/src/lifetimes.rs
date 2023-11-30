c3a535d5074cb76a5b7d20631656b045c3f71c9c
Jubilee Young <workingjubilee@gmail.com>
2023-11-29 16:00:22 -0800

    Apply the same treatment to anonymize_lifetimes

--- pgrx-sql-entity-graph/src/lifetimes.rs
+++ pgrx-sql-entity-graph/src/lifetimes.rs
@@ -93,53 +93,40 @@ pub fn anonymize_lifetimes_in_type_path(value: syn::TypePath) -> syn::TypePath {
 
 pub fn anonymize_lifetimes(value: &mut syn::Type) {
     match value {
-        syn::Type::Path(type_path) => {
-            for segment in &mut type_path.path.segments {
-                if let syn::PathArguments::AngleBracketed(bracketed) = &mut segment.arguments {
-                    for arg in &mut bracketed.args {
-                        match arg {
-                            // rename lifetimes to the anonymous lifetime
-                            syn::GenericArgument::Lifetime(lifetime) => {
-                                lifetime.ident = syn::Ident::new("_", lifetime.ident.span());
-                            }
-
-                            // recurse
-                            syn::GenericArgument::Type(ty) => anonymize_lifetimes(ty),
-                            syn::GenericArgument::Binding(binding) => {
-                                anonymize_lifetimes(&mut binding.ty)
-                            }
-                            syn::GenericArgument::Constraint(constraint) => {
-                                for bound in constraint.bounds.iter_mut() {
-                                    match bound {
-                                        syn::TypeParamBound::Lifetime(lifetime) => {
-                                            lifetime.ident =
-                                                syn::Ident::new("_", lifetime.ident.span())
-                                        }
-                                        _ => {}
-                                    }
-                                }
-                            }
-
-                            // nothing to do otherwise
-                            _ => {}
-                        }
-                    }
+        syn::Type::Path(type_path) => type_path
+            .path
+            .segments
+            .iter_mut()
+            .filter_map(|segment| match &mut segment.arguments {
+                syn::PathArguments::AngleBracketed(bracketed) => Some(bracketed),
+                _ => None,
+            })
+            .flat_map(|bracketed| &mut bracketed.args)
+            .for_each(|arg| match arg {
+                // rename lifetimes to the anonymous lifetime
+                syn::GenericArgument::Lifetime(lifetime) => {
+                    lifetime.ident = syn::Ident::new("_", lifetime.ident.span());
                 }
-            }
-        }
+                // recurse
+                syn::GenericArgument::Type(ty) => anonymize_lifetimes(ty),
+                syn::GenericArgument::Binding(binding) => anonymize_lifetimes(&mut binding.ty),
+                syn::GenericArgument::Constraint(constraint) => {
+                    constraint.bounds.iter_mut().for_each(|bound| {
+                        if let syn::TypeParamBound::Lifetime(lifetime) = bound {
+                            lifetime.ident = syn::Ident::new("_", lifetime.ident.span())
+                        }
+                    })
+                }
+                // nothing to do otherwise
+                _ => {}
+            }),
 
         syn::Type::Reference(type_ref) => {
             if let Some(lifetime) = type_ref.lifetime.as_mut() {
                 lifetime.ident = syn::Ident::new("_", lifetime.ident.span());
             }
         }
-
-        syn::Type::Tuple(type_tuple) => {
-            for elem in &mut type_tuple.elems {
-                anonymize_lifetimes(elem);
-            }
-        }
-
+        syn::Type::Tuple(type_tuple) => type_tuple.elems.iter_mut().for_each(anonymize_lifetimes),
         _ => {}
     }
 }
