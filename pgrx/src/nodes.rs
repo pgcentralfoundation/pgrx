45c23735b9ee5d8910ea3cf5f23ca8da0c7dbce7
lokathor <zefria@gmail.com>
2023-12-26 22:12:41 -0700

    document `is_a` and avoid creating an intermediate reference we don't need

--- pgrx/src/nodes.rs
+++ pgrx/src/nodes.rs
@@ -13,10 +13,15 @@
 
 use crate::pg_sys;
 
+/// Checks if a node "is a" node of the given tag
+/// ```c
 /// #define IsA(nodeptr,_type_)            (nodeTag(nodeptr) == T_##_type_)
+/// ```
+/// ## Safety
+/// * If `nodeptr` is non-null, it must be aligned and point to a valid `Node`
 #[inline]
 pub unsafe fn is_a(nodeptr: *mut pg_sys::Node, tag: pg_sys::NodeTag) -> bool {
-    !nodeptr.is_null() && nodeptr.as_ref().unwrap().type_ == tag
+    !nodeptr.is_null() && (*nodeptr).type_ == tag
 }
 
 /// Convert a [pg_sys::Node] into its textual representation
