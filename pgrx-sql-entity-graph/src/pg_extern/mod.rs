ef2c0813e7b3b4d50c2522dd500d834bef922f5f
Jubilee Young <workingjubilee@gmail.com>
2024-01-05 17:56:53 -0800

    Remove broken staticize_lifetimes code

--- pgrx-sql-entity-graph/src/pg_extern/mod.rs
+++ pgrx-sql-entity-graph/src/pg_extern/mod.rs
@@ -34,7 +34,6 @@ use search_path::SearchPathList;
 use crate::enrich::CodeEnrichment;
 use crate::enrich::ToEntityGraphTokens;
 use crate::enrich::ToRustCodeTokens;
-use crate::lifetimes::staticize_lifetimes;
 use proc_macro2::{Ident, Span, TokenStream as TokenStream2};
 use quote::{quote, quote_spanned, ToTokens};
 use syn::parse::{Parse, ParseStream, Parser};
@@ -133,12 +132,11 @@ impl PgExtern {
                 match v {
                     syn::FnArg::Receiver(_) => None,
                     syn::FnArg::Typed(pat_ty) => {
-                        let mut static_ty = match UsedType::new(*pat_ty.ty.clone()) {
+                        let ty = match UsedType::new(*pat_ty.ty.clone()) {
                             Ok(v) => v.resolved_ty,
                             Err(e) => return Some(Err(e)),
                         };
-                        staticize_lifetimes(&mut static_ty);
-                        Some(Ok(static_ty))
+                        Some(Ok(ty))
                     }
                 }
             })
@@ -275,9 +273,8 @@ impl PgExtern {
         let return_type = match &self.func.sig.output {
             syn::ReturnType::Default => None,
             syn::ReturnType::Type(arrow, ty) => {
-                let mut static_ty = ty.clone();
-                staticize_lifetimes(&mut static_ty);
-                Some(syn::ReturnType::Type(*arrow, static_ty))
+                let ty = ty.clone();
+                Some(syn::ReturnType::Type(*arrow, ty))
             }
         };
 
