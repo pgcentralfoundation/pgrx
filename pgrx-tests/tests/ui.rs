194105064ae76bb629f658ddcd2048e55decd74f
Jubilee Young <workingjubilee@gmail.com>
2024-01-17 11:33:05 -0800

    Add todo tests as ui tests

--- pgrx-tests/tests/ui.rs
+++ pgrx-tests/tests/ui.rs
@@ -2,8 +2,17 @@
 
 use trybuild::TestCases;
 
+/// These are tests which are intended to always fail.
+
 #[test]
 fn ui() {
     let t = TestCases::new();
     t.compile_fail("tests/ui/*.rs");
+}
+
+/// These are tests which currently fail, but should be fixed later.
+#[test]
+fn todo() {
+    let t = TestCases::new();
+    t.compile_fail("tests/todo/*.rs");
 }
