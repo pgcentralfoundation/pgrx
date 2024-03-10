602e592e318059e2cdf23e28062983214ddc90e0
vados <vados@vadosware.io>
2024-03-10 21:55:27 +0900

    feat(pgrx): add chrono feature for easy conversions
    This commit adds a `chrono` feature flag to `pgrx`, which enables
    conversions between `pgrx` native date/time (with or without timezone)
    and `chrono` types like `NaiveDateTime`.

---
+++ pgrx/src/datum/datetime_support/chrono.rs
@@ -0,0 +1,19 @@
+//! This module contains implementations and functionality that enables [`pgrx`] types (ex. [`pgrx::datum::Date`])
+//! to be converted to [`chrono`] data types (ex. [`chrono::Date`])
+#![cfg(feature = "chrono")]
+
+use std::convert::TryFrom;
+
+use chrono;
+
+use crate::datum::datetime_support::DateTimeConversionError;
+use crate::datum::Date;
+
+impl TryFrom<Date> for chrono::naive::NaiveDate {
+    type Error = DateTimeConversionError;
+
+    fn try_from(d: Date) -> Result<chrono::NaiveDate, DateTimeConversionError> {
+        chrono::NaiveDate::from_ymd_opt(d.year(), d.month().into(), d.day().into())
+            .ok_or_else(|| DateTimeConversionError::InvalidFormat)
+    }
+}
