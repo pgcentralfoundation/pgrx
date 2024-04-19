7afd1bbdf5624dd6d3dec046dac5d75adee2d064
Jubilee Young <workingjubilee@gmail.com>
2024-04-19 16:02:48 -0700

    Silence, paperclip

--- pgrx-sql-entity-graph/src/finfo.rs
+++ pgrx-sql-entity-graph/src/finfo.rs
@@ -1,11 +1,11 @@
 use quote::{format_ident, quote};
-use syn;
+use syn::{self, ItemFn};
 
 /// Generate the Postgres fn info record
 ///
 /// Equivalent to PG_FUNCTION_INFO_V1, Postgres will sprintf the fn ident, then `dlsym(so, expected_name)`,
 /// so it is important to pass exactly the ident that you want to have the record associated with!
-pub fn finfo_v1_tokens(ident: proc_macro2::Ident) -> syn::Result<syn::ItemFn> {
+pub fn finfo_v1_tokens(ident: proc_macro2::Ident) -> syn::Result<ItemFn> {
     let finfo_name = format_ident!("pg_finfo_{ident}");
     let tokens = quote! {
         #[no_mangle]
