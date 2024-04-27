13cf7087b368a426562cb58cb49868e51e29dbb4
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-04-27 18:26:20 -0500

    feat(nix): provide `buildPgrxExtension` function
    This function can be used by external nix flakes to build an extension
    using pgrx. The caller must provide the postgres package, the rust
    toolchain, extension source code, and flake system that they are
    building for.
    
    Most of this configuration has been converted from the previous nix
    flake exposed from pgrx. I've made every attempt to trim away parts that
    don't serve a purpose, but there's a chance that this could be
    simplified slightly by someone who has more knowledge of pgrx than I do.

--- flake.nix
+++ flake.nix
@@ -45,5 +45,84 @@
           };
         };
       };
+
+      flake.lib.buildPgrxExtension = {
+        rustToolchain,
+        system,
+        src,
+        postgresql,
+        additionalFeatures ? [],
+      }: let
+        cargo-pgrx = inputs.self.packages.${system}.cargo-pgrx;
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        craneLib = inputs.crane.lib.${system}.overrideToolchain rustToolchain;
+
+        postgresMajor = inputs.nixpkgs.lib.versions.major postgresql.version;
+        cargoToml = builtins.fromTOML (builtins.readFile "${src}/Cargo.toml");
+        name = cargoToml.package.name;
+        pgrxFeatures = builtins.toString additionalFeatures;
+
+        preBuildAndTest = ''
+          export PGRX_HOME=$(mktemp -d)
+          mkdir -p $PGRX_HOME/${postgresMajor}
+
+          cp -r -L ${postgresql}/. $PGRX_HOME/${postgresMajor}/
+          chmod -R ugo+w $PGRX_HOME/${postgresMajor}
+          cp -r -L ${postgresql.lib}/lib/. $PGRX_HOME/${postgresMajor}/lib/
+
+          ${cargo-pgrx}/bin/cargo-pgrx pgrx init \
+            --pg${postgresMajor} $PGRX_HOME/${postgresMajor}/bin/pg_config \
+        '';
+
+        craneCommonBuildArgs = {
+          inherit src;
+          pname = "${name}-pg${postgresMajor}";
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.rustPlatform.bindgenHook
+            postgresql.lib
+            postgresql
+          ];
+          cargoExtraArgs = "--no-default-features --features \"pg${postgresMajor} ${pgrxFeatures}\"";
+          postPatch = "patchShebangs .";
+          preBuild = preBuildAndTest;
+          preCheck = preBuildAndTest;
+          postBuild = ''
+            if [ -f "${name}.control" ]; then
+              export NIX_PGLIBDIR=${postgresql.out}/share/postgresql/extension/
+              ${cargo-pgrx}/bin/cargo-pgrx pgrx package --pg-config ${postgresql}/bin/pg_config --features "${pgrxFeatures}" --out-dir $out
+              export NIX_PGLIBDIR=$PGRX_HOME/${postgresMajor}/lib
+            fi
+          '';
+
+          PGRX_PG_SYS_SKIP_BINDING_REWRITE = "1";
+          CARGO = "${rustToolchain}/bin/cargo";
+          CARGO_BUILD_INCREMENTAL = "false";
+          RUST_BACKTRACE = "full";
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly craneCommonBuildArgs;
+      in
+        craneLib.mkCargoDerivation ({
+            inherit cargoArtifacts;
+            buildPhaseCargoCommand = ''
+              ${cargo-pgrx}/bin/cargo-pgrx pgrx package --pg-config ${postgresql}/bin/pg_config --features "${pgrxFeatures}" --out-dir $out
+            '';
+            doCheck = false;
+            preFixup = ''
+              if [ -f "${name}.control" ]; then
+                ${cargo-pgrx}/bin/cargo-pgrx pgrx stop all
+                rm -rfv $out/target*
+              fi
+            '';
+
+            postInstall = ''
+              mkdir -p $out/lib
+              cp target/release/lib${name}.so $out/lib/${name}.so
+              mv -v $out/${postgresql.out}/* $out
+              rm -rfv $out/nix
+            '';
+          }
+          // craneCommonBuildArgs);
     };
 }
