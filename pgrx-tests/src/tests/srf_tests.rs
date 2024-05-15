fe15aad47584a590d96f4d5cff649fed4629d7ec
Jubilee Young <workingjubilee@gmail.com>
2024-05-14 18:08:51 -0700

    Convert iter-over-tuple-with-result into result-of-iter

--- pgrx-tests/src/tests/srf_tests.rs
+++ pgrx-tests/src/tests/srf_tests.rs
@@ -280,13 +280,21 @@ mod tests {
 
     #[pg_test(error = "column \"cause_an_error\" does not exist")]
     pub fn spi_in_iterator(
-    ) -> TableIterator<'static, (name!(id, i32), name!(relname, Result<Option<String>, spi::Error>))>
+    ) -> Result<TableIterator<'static, (name!(id, i32), name!(relname, Option<String>))>, spi::Error>
     {
         let oids = vec![1213, 1214, 1232, 1233, 1247, 1249, 1255];
-
-        TableIterator::new(oids.into_iter().map(|oid| {
-            (oid, Spi::get_one(&format!("SELECT CAUSE_AN_ERROR FROM pg_class WHERE oid = {oid}")))
-        }))
+        let result = oids
+            .into_iter()
+            .map(|oid| {
+                Ok((
+                    oid,
+                    Spi::get_one(&format!(
+                        "SELECT CAUSE_AN_ERROR FROM pg_class WHERE oid = {oid}"
+                    ))?,
+                ))
+            })
+            .collect::<Result<Vec<_>, _>>();
+        result.map(|v| TableIterator::new(v))
     }
 
     #[pg_test(error = "column \"cause_an_error\" does not exist")]
