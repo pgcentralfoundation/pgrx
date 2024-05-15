a32398ff1816560064088a17f2badc896cef7dfb
Jubilee Young <workingjubilee@gmail.com>
2024-05-15 15:33:32 -0700

    Remove clippy warning that no longer applies

--- pgrx/src/iter.rs
+++ pgrx/src/iter.rs
@@ -7,7 +7,6 @@
 //LICENSE All rights reserved.
 //LICENSE
 //LICENSE Use of this source code is governed by the MIT license that can be found in the LICENSE file.
-#![allow(clippy::vec_init_then_push)]
 use core::{iter, ptr};
 
 use crate::callconv::{CallCx, Ret, RetPackage, ReturnShipping};
