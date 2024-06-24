efb7c46da5a6427f4188080638ab6521a684cc70
Emily Cutlip <4525370+NotGyro@users.noreply.github.com>
2024-06-24 15:07:48 -0400

    Added nightly compile-fail and todo tests.

--- pgrx-tests/tests/ui.rs
+++ pgrx-tests/tests/ui.rs
@@ -1,17 +1,35 @@
-#![cfg(all(not(target_env = "musl"), not(feature = "nightly")))]
+#![cfg(not(target_env = "musl"))]
 
 use trybuild::TestCases;
 
 /// These are tests which are intended to always fail.
+#[cfg(not(feature = "nightly"))]
 #[test]
 fn compile_fail() {
     let t = TestCases::new();
-    t.compile_fail("tests/compile-fail/*.rs");
+    t.compile_fail("tests/stable/compile-fail/*.rs");
 }
 
 /// These are tests which currently fail, but should be fixed later.
+#[cfg(not(feature = "nightly"))]
 #[test]
 fn todo() {
     let t = TestCases::new();
-    t.compile_fail("tests/todo/*.rs");
+    t.compile_fail("tests/stable/todo/*.rs");
+}
+
+/// These are tests which are intended to always fail.
+#[cfg(feature = "nightly")]
+#[test]
+fn compile_fail() {
+    let t = TestCases::new();
+    t.compile_fail("tests/nightly/compile-fail/*.rs");
+}
+
+/// These are tests which currently fail, but should be fixed later.
+#[cfg(feature = "nightly")]
+#[test]
+fn todo() {
+    let t = TestCases::new();
+    t.compile_fail("tests/nightly/todo/*.rs");
 }
