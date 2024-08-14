5b53fed1c776888164f68d244738e7ad16a323d8
Jubilee Young <workingjubilee@gmail.com>
2024-08-14 07:00:23 -0700

    use C-header-recommended options

--- pgrx-bindgen/src/build.rs
+++ pgrx-bindgen/src/build.rs
@@ -777,6 +777,7 @@ fn run_bindgen(
         .merge_extern_blocks(true)
         .wrap_unsafe_ops(true)
         .use_core()
+        .disable_nested_struct_naming()
         .formatter(bindgen::Formatter::None)
         .layout_tests(false)
         .default_non_copy_union_style(NonCopyUnionStyle::ManuallyDrop)
