fd8a883889968adeb93125cc629e23053fb0cfde
Tim Waizenegger <tim.waizenegger@enterprisedb.com>
2024-08-14 11:10:19 +0200

    escape the .sql

--- cargo-pgrx/src/command/install.rs
+++ cargo-pgrx/src/command/install.rs
@@ -392,7 +392,7 @@ fn copy_sql_files(
 
             // match the required pattern `extension--old_version--target_version.sql`
             let re_update_script_name =
-                regex::Regex::new(&format!(r"^{extname}--.+--.+.sql$")).unwrap();
+                regex::Regex::new(&format!(r"^{extname}--.+--.+\.sql$")).unwrap();
 
             if re_update_script_name.is_match(filename.as_str()) {
                 let mut dest = base_directory.to_path_buf();
