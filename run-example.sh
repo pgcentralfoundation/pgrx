fb6e763fdf7fc484657b741bed81828d4fccf50f
Jubilee Young <workingjubilee@gmail.com>
2024-08-20 12:04:17 -0700

    update script

--- run-example.sh
+++ run-example.sh
@@ -1,1 +1,10 @@
-CARGO_TARGET_DIR="$(pwd)/target" cargo test --manifest-path=pgrx-examples/"${1}"/Cargo.toml  --features "pg${PG_VER:-14}" --no-default-features
+function exec_example() {
+  CARGO_TARGET_DIR="$(pwd)/target" cargo test --manifest-path="${1}"/Cargo.toml  --features "pg${PG_VER:-14}" --no-default-features
+}
+if [ $1 = "all" ]; then
+  for example in pgrx-examples/*; do
+      exec_example "$example"
+  done
+else
+  exec_example "pgrx-examples/$1"
+fi
