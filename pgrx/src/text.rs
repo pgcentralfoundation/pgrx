fac7f1ef2caca8e42d24ea634d4f2d155cea62a4
Jubilee Young <workingjubilee@gmail.com>
2024-09-26 15:18:31 -0700

    Address the TODO for Text

--- pgrx/src/text.rs
+++ pgrx/src/text.rs
@@ -1,11 +1,18 @@
-use crate::datum::{BorrowDatum, Datum};
+use crate::datum::BorrowDatum;
 use crate::layout::PassBy;
 use crate::pgrx_sql_entity_graph::metadata::{
     ArgumentError, Returns, ReturnsError, SqlMapping, SqlTranslatable,
 };
 use crate::{pg_sys, varlena};
-use core::ops::{Deref, DerefMut};
-use core::ptr;
+use core::borrow::Borrow;
+use core::{ptr, slice, str};
+
+use bstr::{BStr, ByteSlice};
+
+// We reexport these types so people don't have to care whether they're pulled from BStr or std,
+// they just use the ones from pgrx::text::*
+pub use bstr::{Bytes, Chars};
+pub use core::str::{Utf8Chunks, Utf8Error};
 
 /// A Postgres string, AKA `TEXT`.
 ///
@@ -14,25 +21,55 @@ use core::ptr;
 #[repr(transparent)]
 pub struct Text([u8]);
 
-// TODO(0.12.0): strip this and make Text forward its impl to BStr fn instead
-impl Deref for Text {
-    type Target = str;
-    fn deref(&self) -> &str {
+impl Text {
+    pub fn as_bytes(&self) -> &[u8] {
         let self_ptr = self as *const Text as *const pg_sys::varlena;
-        unsafe { varlena::text_to_rust_str(self_ptr).unwrap() }
-    }
-}
+        unsafe {
+            let len = varlena::varsize_any_exhdr(self_ptr);
+            let data = varlena::vardata_any(self_ptr);
 
-// TODO(0.12.0): strip this and make Text forward its impl to BStr fn instead
-impl DerefMut for Text {
-    fn deref_mut(&mut self) -> &mut str {
+            slice::from_raw_parts(data.cast::<u8>(), len)
+        }
+    }
+
+    /// Manipulate Text as its byte repr
+    ///
+    /// # Safety
+    /// Like [`str::as_bytes_mut`], this can cause problems if you change Text in a way that
+    /// your database is not specified to support, so the caller must assure that it remains in
+    /// a valid encoding for the database.
+    pub unsafe fn as_bytes_mut(&mut self) -> &mut [u8] {
         let self_ptr = self as *mut Text as *mut pg_sys::varlena;
         unsafe {
             let len = varlena::varsize_any_exhdr(self_ptr);
             let data = varlena::vardata_any(self_ptr);
 
-            &mut *(ptr::slice_from_raw_parts_mut(data as *mut u8, len) as *mut str)
+            slice::from_raw_parts_mut(data.cast::<u8>().cast_mut(), len)
         }
+    }
+
+    /// Reborrow `&Text as `&BStr`
+    ///
+    /// We do not implement Deref to BStr or [u8] because we'd like to expose a more selective API.
+    /// Several fn that [u8] implements are implemented very differently on str!
+    fn as_bstr(&self) -> &BStr {
+        self.as_bytes().borrow()
+    }
+
+    pub fn chars(&self) -> Chars<'_> {
+        self.as_bstr().chars()
+    }
+
+    pub fn bytes(&self) -> Bytes<'_> {
+        self.as_bstr().bytes()
+    }
+
+    pub fn to_str(&self) -> Result<&str, Utf8Error> {
+        str::from_utf8(self.as_bytes())
+    }
+
+    pub fn utf8_chunks(&self) -> Utf8Chunks {
+        self.as_bytes().utf8_chunks()
     }
 }
 
