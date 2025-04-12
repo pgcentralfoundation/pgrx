62343387cef5fc2082032dee15d9a0a36db0bb36
Eric B. Ridge <eebbrr@paradedb.com>
2025-04-12 11:16:17 -0400

    remove pg12 references

--- pgrx/src/rel.rs
+++ pgrx/src/rel.rs
@@ -20,7 +20,6 @@ use std::os::raw::c_char;
 macro_rules! pgstat_count_impl {
     ($name:ident, $new_field:ident, $old_field:ident) => {
         pub fn $name(&mut self) {
-            #[cfg(not(feature = "pg12"))]
             if self.should_count_relation() {
                 let info = self.pgstat_info;
 
@@ -312,7 +311,6 @@ impl PgRelation {
     }
 
     #[inline(always)]
-    #[cfg(not(feature = "pg12"))]
     fn should_count_relation(&mut self) -> bool {
         if !self.pgstat_info.is_null() {
             return true;
@@ -339,9 +337,8 @@ impl PgRelation {
 
     pub fn count_index_tuples(
         &mut self,
-        #[cfg_attr(feature = "pg12", allow(unused_variables))] n: i64,
+        n: i64,
     ) {
-        #[cfg(not(feature = "pg12"))]
         if self.should_count_relation() {
             let info = self.pgstat_info;
             #[cfg(any(feature = "pg16", feature = "pg17"))]
