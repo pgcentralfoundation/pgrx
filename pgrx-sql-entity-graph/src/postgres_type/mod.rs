011f88e3732944574e994d33ab83a5cb54b91b31
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-12 19:58:32 +0200

    Removed unnecessary option wrap

--- pgrx-sql-entity-graph/src/postgres_type/mod.rs
+++ pgrx-sql-entity-graph/src/postgres_type/mod.rs
@@ -215,20 +215,20 @@ impl ToEntityGraphTokens for PostgresTypeDerive {
                         let _ = path_items.pop(); // Drop the one we don't want.
                         path_items.join("::")
                     },
-                    receive_fn: Some(stringify!(#receive_fn)),
-                    receive_fn_module_path: Some({
+                    receive_fn: stringify!(#receive_fn),
+                    receive_fn_module_path: {
                         let recv_fn = stringify!(#receive_fn);
                         let mut path_items: Vec<_> = recv_fn.split("::").collect();
                         let _ = path_items.pop(); // Drop the one we don't want.
                         path_items.join("::")
-                    }),
-                    send_fn: Some(stringify!(#send_fn)),
-                    send_fn_module_path: Some({
+                    },
+                    send_fn: stringify!(#send_fn),
+                    send_fn_module_path: {
                         let send_fn = stringify!(#send_fn);
                         let mut path_items: Vec<_> = send_fn.split("::").collect();
                         let _ = path_items.pop(); // Drop the one we don't want.
                         path_items.join("::")
-                    }),
+                    },
                     to_sql_config: #to_sql_config,
                     alignment: #alignment,
                 };
