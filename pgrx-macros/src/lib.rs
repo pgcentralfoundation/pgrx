b643f4d8c6a84722fc1408e5fe3d9213b491a5fa
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-12 22:33:37 +0200

    Testing

--- pgrx-macros/src/lib.rs
+++ pgrx-macros/src/lib.rs
@@ -956,7 +956,7 @@ fn impl_postgres_type(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream>
 
             #[doc(hidden)]
             #[::pgrx::pgrx_macros::pg_extern(immutable, parallel_safe)]
-            pub fn #funcname_recv #generics(internal: ::pgrx::datum::Internal) -> #name #generics {
+            pub fn #funcname_recv #generics(internal: ::pgrx::datum::Internal) -> Option<#name #generics> {
                 let string_info = unsafe {
                     let data = internal.get_mut::<::pgrx::pg_sys::StringInfoData>();
                     ::pgrx::StringInfo::from_pg(data.expect("internal input pointer is NULL"))
@@ -964,7 +964,7 @@ fn impl_postgres_type(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream>
                 .expect("failed to create StringInfo from internal");
 
                 let bytes = string_info.as_bytes();
-                serde_cbor::from_slice(bytes).expect("failed to decode from CBOR")
+                serde_cbor::from_slice(bytes).ok()
             }
             
             #[doc(hidden)]
