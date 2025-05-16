2a19e1ea75007e50eac9d5a37173d6485a842a16
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-16 15:27:15 +0200

    Debugging - 1

--- pgrx/src/datum/varlena.rs
+++ pgrx/src/datum/varlena.rs
@@ -401,6 +401,7 @@ pub unsafe fn cbor_decode<'de, T>(input: *mut pg_sys::varlena) -> T
 where
     T: Deserialize<'de>,
 {
+    todo!("cbor_decode stops here!");
     let varlena = pg_sys::pg_detoast_datum_packed(input as *mut pg_sys::varlena);
     let len = varsize_any_exhdr(varlena);
     let data = vardata_any(varlena);
