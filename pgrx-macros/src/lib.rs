511e1310e383bb6432648f969a753a7d1c9c3c62
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-16 20:13:38 +0200

    Trying a different approach to create varlena using StringInfo

--- pgrx-macros/src/lib.rs
+++ pgrx-macros/src/lib.rs
@@ -1013,17 +1013,18 @@ fn impl_postgres_type(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream>
         ) -> #name #generics {
             let buf = unsafe { internal.get_mut::<::pgrx::pg_sys::StringInfoData>().unwrap() };
 
-            let len_including_header = buf.len + ::pgrx::pg_sys::VARHDRSZ as ::core::ffi::c_int;
-            let shifted_len_including_header = len_including_header << 2;
-            let header: [u8; 4] = shifted_len_including_header.to_le_bytes();
+            let mut serialized = StringInfo::new();
+
+            serialized.push_bytes(&[0u8; pg_sys::VARHDRSZ]); // reserve space for the header
+            serialized.push_bytes(buf.data);
+
+            let size = serialized.len();
+            let varlena = serialized.into_char_ptr();
 
             unsafe{
-                let mut varlena = ::pgrx::pg_sys::varlena {
-                    vl_len_: core::mem::transmute(header),
-                    vl_dat: core::mem::transmute(buf.data),
-                };
+                ::pgrx::set_varsize_4b(varlena as *mut ::pgrx::pg_sys::varlena, size as i32);
                 buf.cursor = buf.len;
-                ::pgrx::datum::cbor_decode((&mut varlena) as *mut ::pgrx::pg_sys::varlena)
+                ::pgrx::datum::cbor_decode(varlena as *mut ::pgrx::pg_sys::varlena)
             }
         }
         #[doc(hidden)]
