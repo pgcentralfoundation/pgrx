1b1625da90ee87920907353f0e0afec3ff182891
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-16 14:27:02 +0200

    Decommented part of the `recv` structure

--- pgrx-macros/src/lib.rs
+++ pgrx-macros/src/lib.rs
@@ -1011,10 +1011,10 @@ fn impl_postgres_type(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream>
         pub fn #funcname_recv #generics(
             internal: ::pgrx::datum::Internal,
         ) -> #name #generics {
-            // use ::pgrx::datum::{FromDatum, IntoDatum};
-            // let Some(datum): Option<::pgrx::pg_sys::Datum> = internal.into_datum() else {
-            //     ::pgrx::error!("Datum of type `{}` is unexpectedly NULL.", stringify!(#name));
-            // };
+            use ::pgrx::datum::{FromDatum, IntoDatum};
+            let Some(datum): Option<::pgrx::pg_sys::Datum> = internal.into_datum() else {
+                ::pgrx::error!("Datum of type `{}` is unexpectedly NULL.", stringify!(#name));
+            };
             // unsafe {
             //     let Some(object) = FromDatum::from_datum(datum, false) else {
             //         ::pgrx::error!("Failed to CBOR-deserialize Datum to type `{}`.", stringify!(#name));
