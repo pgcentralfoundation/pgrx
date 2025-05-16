42d4b462d0d8d8cf05e29aab3aa5f4a4be9cfc66
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2025-05-16 14:39:08 +0200

    Uncommented another portion of the `recv` function

--- pgrx-macros/src/lib.rs
+++ pgrx-macros/src/lib.rs
@@ -1015,12 +1015,11 @@ fn impl_postgres_type(ast: DeriveInput) -> syn::Result<proc_macro2::TokenStream>
             let Some(datum): Option<::pgrx::pg_sys::Datum> = internal.into_datum() else {
                 ::pgrx::error!("Datum of type `{}` is unexpectedly NULL.", stringify!(#name));
             };
-            // unsafe {
-            //     let Some(object) = FromDatum::from_datum(datum, false) else {
-            //         ::pgrx::error!("Failed to CBOR-deserialize Datum to type `{}`.", stringify!(#name));
-            //     };
-            //     object
-            // }
+            unsafe {
+                let Some(object) = FromDatum::from_datum(datum, false) else {
+                    ::pgrx::error!("Failed to CBOR-deserialize Datum to type `{}`.", stringify!(#name));
+                };
+            }
             todo!("implement `recv` function for `{}`", stringify!(#name))
         }
         #[doc(hidden)]
