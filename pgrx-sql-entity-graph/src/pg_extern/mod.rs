d59fe70985e8323611e8b8a93bbad7a73a9f7ebb
Eric B. Ridge <eebbrr@paradedb.com>
2025-08-21 10:44:07 -0400

    give exported `__pgrx_internals_fn_*` functions unique names
    It's entirely possible for a pgrx extension to have functions like this:
    
    ```rust
    mod a {
       #[pg_extern]
       fn foo() { }
    }
    
    mod b {
       #[pg_extern]
       fn foo() { }
    }
    ```
    
    These are two distinct functions in Rust, and should be in the SQL
    schema, but the naming of the sql entity graph `__pgrx_internals_fn_*`
    function isn't unique enough.
    
    We could also include the schema name there, but I don't think that's
    quite enough, so instead we just append a UUIDv4 value.

--- pgrx-sql-entity-graph/src/pg_extern/mod.rs
+++ pgrx-sql-entity-graph/src/pg_extern/mod.rs
@@ -356,7 +356,11 @@ impl PgExtern {
             .collect::<Vec<_>>();
         let hrtb = if lifetimes.is_empty() { None } else { Some(quote! { for<#(#lifetimes),*> }) };
 
-        let sql_graph_entity_fn_name = format_ident!("__pgrx_internals_fn_{}", ident);
+        let uuid = uuid::Uuid::new_v4();
+        let sql_graph_entity_fn_name = Ident::new(
+            &format!("__pgrx_internals_fn_{}_{}", ident, uuid.as_simple()),
+            Span::call_site(),
+        );
         quote_spanned! { self.func.sig.span() =>
             #[unsafe(no_mangle)]
             #[doc(hidden)]
