9b63ca9a7932c58f4b8b800e4313675c27180a1b
Jubilee Young <workingjubilee@gmail.com>
2025-11-18 19:03:42 -0800

    add BorrowDatum for TimeTz

--- pgrx/src/datum/borrow.rs
+++ pgrx/src/datum/borrow.rs
@@ -116,7 +116,7 @@ macro_rules! impl_borrow_fixed_len {
 impl_borrow_fixed_len! {
     i8, i16, i32, i64, bool, f32, f64,
     pg_sys::Oid, pg_sys::Point,
-    Date, Time, Timestamp, TimestampWithTimeZone
+    Date, Time, Timestamp, TimestampWithTimeZone, TimeWithTimeZone
 }
 
 /// It is rare to pass CStr via Datums, but not unheard of
