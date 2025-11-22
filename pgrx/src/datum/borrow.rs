1082f752e2ae80b11823296048ccba98db1c03cc
Jubilee <workingjubilee@gmail.com>
2025-11-22 09:25:35 -0800

    rearrange

--- pgrx/src/datum/borrow.rs
+++ pgrx/src/datum/borrow.rs
@@ -116,7 +116,7 @@ macro_rules! impl_borrow_fixed_len {
 impl_borrow_fixed_len! {
     i8, i16, i32, i64, bool, f32, f64,
     pg_sys::Oid, pg_sys::Point,
-    Date, Time, Timestamp, TimestampWithTimeZone, TimeWithTimeZone
+    Date, Time, TimeWithTimeZone, Timestamp, TimestampWithTimeZone
 }
 
 /// It is rare to pass CStr via Datums, but not unheard of
