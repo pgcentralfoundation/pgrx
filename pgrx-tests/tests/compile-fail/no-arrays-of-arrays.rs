681b2bd153013bf188be453b8d0d502593dc1267
Jubilee Young <workingjubilee@gmail.com>
2025-11-28 19:08:28 -0800

    remove a useless warning

--- pgrx-tests/tests/compile-fail/no-arrays-of-arrays.rs
+++ pgrx-tests/tests/compile-fail/no-arrays-of-arrays.rs
@@ -4,7 +4,7 @@ use pgrx::palloc::PBox;
 use pgrx::prelude::*;
 
 #[pg_extern]
-fn something<'a>(mcx: &MemCx<'a>) -> PBox<'a, FlatArray<'a, FlatArray<'a, i32>>> {
+fn something<'a>(_mcx: &MemCx<'a>) -> PBox<'a, FlatArray<'a, FlatArray<'a, i32>>> {
     todo!()
 }
 
