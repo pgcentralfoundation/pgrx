64e0d33aa5ee47155ab9530695abd01e1dbb071a
Chris Bandy <bandy.chris@gmail.com>
2026-04-11 23:15:01 +0000

    Include "libpq/oauth.h" for OAuth validators
    OAuth validator modules are shared libraries that validate OAuth tokens
    as part of the `oauth` authentication method introduced in Postgres 18.
    
    When a connection matches an `oauth` HBA rule, Postgres loads the one of
    the modules listed in the `oauth_validator_libraries` GUC and calls its
    exported `_PG_oauth_validator_module_init` function.
    
    That function must return a populated `OAuthValidatorCallbacks` struct
    with the module's compile-time `PG_OAUTH_VALIDATOR_MAGIC` constant.
    These and other validator types are defined in `libpq/oauth.h`.
    
    See: https://www.postgresql.org/docs/current/auth-oauth.html
    See: https://www.postgresql.org/docs/current/oauth-validators.html

--- pgrx-pg-sys/include/pg18.h
+++ pgrx-pg-sys/include/pg18.h
@@ -101,6 +101,7 @@
 #include "foreign/foreign.h"
 #include "jit/jit.h"
 #include "lib/stringinfo.h"
+#include "libpq/oauth.h"
 #include "libpq/pqformat.h"
 #include "mb/pg_wchar.h"
 #include "nodes/execnodes.h"
