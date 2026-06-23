50943db2c84f34d9ec65c34ae39fe4b0207d97df
Eric B. Ridge <eebbrr@gmail.com>
2026-06-23 19:46:13 +0000

    Update version to 0.19.1

--- pgrx-pg-sys/src/include/pg19.rs
+++ pgrx-pg-sys/src/include/pg19.rs
@@ -47,7 +47,11 @@ where
     fn change_bit(byte: u8, index: usize, val: bool) -> u8 {
         let bit_index = if cfg!(target_endian = "big") { 7 - (index % 8) } else { index % 8 };
         let mask = 1 << bit_index;
-        if val { byte | mask } else { byte & !mask }
+        if val {
+            byte | mask
+        } else {
+            byte & !mask
+        }
     }
     #[inline]
     pub fn set_bit(&mut self, index: usize, val: bool) {
@@ -37044,7 +37048,7 @@ unsafe extern "C-unwind" {
     ) -> *mut ::core::ffi::c_void;
     pub fn MemoryContextAllocHuge(context: MemoryContext, size: Size) -> *mut ::core::ffi::c_void;
     pub fn repalloc_huge(pointer: *mut ::core::ffi::c_void, size: Size)
-    -> *mut ::core::ffi::c_void;
+        -> *mut ::core::ffi::c_void;
     pub fn MemoryContextRegisterResetCallback(
         context: MemoryContext,
         cb: *mut MemoryContextCallback,
@@ -37299,12 +37303,12 @@ unsafe extern "C-unwind" {
     pub fn verify_compact_attribute(arg1: TupleDesc, attnum: ::core::ffi::c_int);
     #[link_name = "TupleDescCompactAttr__pgrx_cshim"]
     pub fn TupleDescCompactAttr(tupdesc: TupleDesc, i: ::core::ffi::c_int)
-    -> *mut CompactAttribute;
+        -> *mut CompactAttribute;
     pub fn CreateTemplateTupleDesc(natts: ::core::ffi::c_int) -> TupleDesc;
     pub fn CreateTupleDesc(natts: ::core::ffi::c_int, attrs: *mut Form_pg_attribute) -> TupleDesc;
     pub fn CreateTupleDescCopy(tupdesc: TupleDesc) -> TupleDesc;
     pub fn CreateTupleDescTruncatedCopy(tupdesc: TupleDesc, natts: ::core::ffi::c_int)
-    -> TupleDesc;
+        -> TupleDesc;
     pub fn CreateTupleDescCopyConstr(tupdesc: TupleDesc) -> TupleDesc;
     pub fn TupleDescCopy(dst: TupleDesc, src: TupleDesc);
     pub fn TupleDescCopyEntry(
@@ -37589,7 +37593,7 @@ unsafe extern "C-unwind" {
     pub fn OidFunctionCall0Coll(functionId: Oid, collation: Oid) -> Datum;
     pub fn OidFunctionCall1Coll(functionId: Oid, collation: Oid, arg1: Datum) -> Datum;
     pub fn OidFunctionCall2Coll(functionId: Oid, collation: Oid, arg1: Datum, arg2: Datum)
-    -> Datum;
+        -> Datum;
     pub fn OidFunctionCall3Coll(
         functionId: Oid,
         collation: Oid,
@@ -38652,7 +38656,7 @@ unsafe extern "C-unwind" {
     ) -> LockTupleMode::Type;
     pub fn ExecFindRowMark(estate: *mut EState, rti: Index, missing_ok: bool) -> *mut ExecRowMark;
     pub fn ExecBuildAuxRowMark(erm: *mut ExecRowMark, targetlist: *mut List)
-    -> *mut ExecAuxRowMark;
+        -> *mut ExecAuxRowMark;
     pub fn EvalPlanQual(
         epqstate: *mut EPQState,
         relation: Relation,
@@ -40065,7 +40069,7 @@ unsafe extern "C-unwind" {
     pub fn get_backup_status() -> SessionBackupState::Type;
     #[link_name = "FullTransactionIdFromEpochAndXid__pgrx_cshim"]
     pub fn FullTransactionIdFromEpochAndXid(epoch: uint32, xid: TransactionId)
-    -> FullTransactionId;
+        -> FullTransactionId;
     #[link_name = "FullTransactionIdFromU64__pgrx_cshim"]
     pub fn FullTransactionIdFromU64(value: uint64) -> FullTransactionId;
     #[link_name = "FullTransactionIdRetreat__pgrx_cshim"]
@@ -41120,7 +41124,7 @@ unsafe extern "C-unwind" {
         bit: *mut uint8,
     );
     pub fn heap_attisnull(tup: HeapTuple, attnum: ::core::ffi::c_int, tupleDesc: TupleDesc)
-    -> bool;
+        -> bool;
     pub fn nocachegetattr(
         tup: HeapTuple,
         attnum: ::core::ffi::c_int,
@@ -41217,7 +41221,7 @@ unsafe extern "C-unwind" {
     pub fn tbm_end_private_iterate(iterator: *mut TBMPrivateIterator);
     pub fn tbm_end_shared_iterate(iterator: *mut TBMSharedIterator);
     pub fn tbm_attach_shared_iterate(dsa: *mut dsa_area, dp: dsa_pointer)
-    -> *mut TBMSharedIterator;
+        -> *mut TBMSharedIterator;
     pub fn tbm_calculate_entries(maxbytes: Size) -> ::core::ffi::c_int;
     pub fn tbm_begin_iterate(
         tbm: *mut TIDBitmap,
@@ -41240,7 +41244,7 @@ unsafe extern "C-unwind" {
     ) -> ::core::ffi::c_int;
     pub fn set_spins_per_delay(shared_spins_per_delay: ::core::ffi::c_int);
     pub fn update_spins_per_delay(shared_spins_per_delay: ::core::ffi::c_int)
-    -> ::core::ffi::c_int;
+        -> ::core::ffi::c_int;
     #[link_name = "init_spin_delay__pgrx_cshim"]
     pub fn init_spin_delay(
         status: *mut SpinDelayStatus,
@@ -42086,7 +42090,7 @@ unsafe extern "C-unwind" {
     );
     pub fn table_block_parallelscan_estimate(rel: Relation) -> Size;
     pub fn table_block_parallelscan_initialize(rel: Relation, pscan: ParallelTableScanDesc)
-    -> Size;
+        -> Size;
     pub fn table_block_parallelscan_reinitialize(rel: Relation, pscan: ParallelTableScanDesc);
     pub fn table_block_parallelscan_nextpage(
         rel: Relation,
@@ -43035,7 +43039,7 @@ unsafe extern "C-unwind" {
     pub static mut replorigin_xact_state: ReplOriginXactState;
     pub static mut max_active_replication_origins: ::core::ffi::c_int;
     pub fn replorigin_by_name(roname: *const ::core::ffi::c_char, missing_ok: bool)
-    -> ReplOriginId;
+        -> ReplOriginId;
     pub fn replorigin_create(roname: *const ::core::ffi::c_char) -> ReplOriginId;
     pub fn replorigin_drop_by_name(
         name: *const ::core::ffi::c_char,
@@ -44122,7 +44126,7 @@ unsafe extern "C-unwind" {
     pub fn AlterDatabaseRefreshColl(stmt: *mut AlterDatabaseRefreshCollStmt) -> ObjectAddress;
     pub fn AlterDatabaseSet(stmt: *mut AlterDatabaseSetStmt) -> Oid;
     pub fn AlterDatabaseOwner(dbname: *const ::core::ffi::c_char, newOwnerId: Oid)
-    -> ObjectAddress;
+        -> ObjectAddress;
     pub fn have_createdb_privilege() -> bool;
     pub fn check_encoding_locale_matches(
         encoding: ::core::ffi::c_int,
@@ -46303,7 +46307,7 @@ unsafe extern "C-unwind" {
         Nulls: *const ::core::ffi::c_char,
     ) -> HeapTuple;
     pub fn SPI_fnumber(tupdesc: TupleDesc, fname: *const ::core::ffi::c_char)
-    -> ::core::ffi::c_int;
+        -> ::core::ffi::c_int;
     pub fn SPI_fname(tupdesc: TupleDesc, fnumber: ::core::ffi::c_int) -> *mut ::core::ffi::c_char;
     pub fn SPI_getvalue(
         tuple: HeapTuple,
@@ -46317,7 +46321,7 @@ unsafe extern "C-unwind" {
         isnull: *mut bool,
     ) -> Datum;
     pub fn SPI_gettype(tupdesc: TupleDesc, fnumber: ::core::ffi::c_int)
-    -> *mut ::core::ffi::c_char;
+        -> *mut ::core::ffi::c_char;
     pub fn SPI_gettypeid(tupdesc: TupleDesc, fnumber: ::core::ffi::c_int) -> Oid;
     pub fn SPI_getrelname(rel: Relation) -> *mut ::core::ffi::c_char;
     pub fn SPI_getnspname(rel: Relation) -> *mut ::core::ffi::c_char;
@@ -46483,11 +46487,11 @@ unsafe extern "C-unwind" {
     pub fn pg_encoding_max_length(encoding: ::core::ffi::c_int) -> ::core::ffi::c_int;
     pub fn pg_valid_client_encoding(name: *const ::core::ffi::c_char) -> ::core::ffi::c_int;
     pub fn pg_valid_server_encoding_private(name: *const ::core::ffi::c_char)
-    -> ::core::ffi::c_int;
+        -> ::core::ffi::c_int;
     pub fn is_encoding_supported_by_icu(encoding: ::core::ffi::c_int) -> bool;
     pub fn get_encoding_name_for_icu(encoding: ::core::ffi::c_int) -> *const ::core::ffi::c_char;
     pub fn pg_utf8_islegal(source: *const ::core::ffi::c_uchar, length: ::core::ffi::c_int)
-    -> bool;
+        -> bool;
     pub fn pg_utf_mblen_private(s: *const ::core::ffi::c_uchar) -> ::core::ffi::c_int;
     pub fn pg_mb2wchar(from: *const ::core::ffi::c_char, to: *mut pg_wchar) -> ::core::ffi::c_int;
     pub fn pg_mb2wchar_with_len(
@@ -46614,7 +46618,7 @@ unsafe extern "C-unwind" {
         lc: *mut ::core::ffi::c_uchar,
     ) -> ::core::ffi::c_ushort;
     pub fn CNStoBIG5(cns: ::core::ffi::c_ushort, lc: ::core::ffi::c_uchar)
-    -> ::core::ffi::c_ushort;
+        -> ::core::ffi::c_ushort;
     pub fn UtfToLocal(
         utf: *const ::core::ffi::c_uchar,
         len: ::core::ffi::c_int,
@@ -47607,7 +47611,7 @@ unsafe extern "C-unwind" {
     pub fn contain_vars_of_level(node: *mut Node, levelsup: ::core::ffi::c_int) -> bool;
     pub fn contain_vars_returning_old_or_new(node: *mut Node) -> bool;
     pub fn locate_var_of_level(node: *mut Node, levelsup: ::core::ffi::c_int)
-    -> ::core::ffi::c_int;
+        -> ::core::ffi::c_int;
     pub fn pull_var_clause(node: *mut Node, flags: ::core::ffi::c_int) -> *mut List;
     pub fn flatten_join_alias_vars(
         root: *mut PlannerInfo,
@@ -47858,7 +47862,7 @@ unsafe extern "C-unwind" {
         inner_paramrels: Relids,
     ) -> Relids;
     pub fn calc_non_nestloop_required_outer(outer_path: *mut Path, inner_path: *mut Path)
-    -> Relids;
+        -> Relids;
     pub fn create_nestloop_path(
         root: *mut PlannerInfo,
         joinrel: *mut RelOptInfo,
@@ -48125,7 +48129,7 @@ unsafe extern "C-unwind" {
         required_outer: Relids,
     ) -> *mut ParamPathInfo;
     pub fn find_param_path_info(rel: *mut RelOptInfo, required_outer: Relids)
-    -> *mut ParamPathInfo;
+        -> *mut ParamPathInfo;
     pub fn get_param_path_clause_serials(path: *mut Path) -> *mut Bitmapset;
     pub fn build_child_join_rel(
         root: *mut PlannerInfo,
@@ -48857,9 +48861,9 @@ unsafe extern "C-unwind" {
         pushedDown: bool,
     );
     pub fn BuildOnConflictExcludedTargetlist(targetrel: Relation, exclRelIndex: Index)
-    -> *mut List;
+        -> *mut List;
     pub fn makeSortGroupClauseForSetOp(rescoltype: Oid, require_hash: bool)
-    -> *mut SortGroupClause;
+        -> *mut SortGroupClause;
     pub fn constructSetOpTargetlist(
         pstate: *mut ParseState,
         op: *mut SetOperationStmt,
@@ -48873,7 +48877,7 @@ unsafe extern "C-unwind" {
     pub fn assign_list_collations(pstate: *mut ParseState, exprs: *mut List);
     pub fn assign_expr_collations(pstate: *mut ParseState, expr: *mut Node);
     pub fn select_common_collation(pstate: *mut ParseState, exprs: *mut List, none_ok: bool)
-    -> Oid;
+        -> Oid;
     pub static mut Transform_null_equals: bool;
     pub fn transformExpr(
         pstate: *mut ParseState,
@@ -49543,7 +49547,7 @@ unsafe extern "C-unwind" {
     );
     pub fn RelationGetPartitionDesc(rel: Relation, omit_detached: bool) -> PartitionDesc;
     pub fn CreatePartitionDirectory(mcxt: MemoryContext, omit_detached: bool)
-    -> PartitionDirectory;
+        -> PartitionDirectory;
     pub fn PartitionDirectoryLookup(arg1: PartitionDirectory, arg2: Relation) -> PartitionDesc;
     pub fn DestroyPartitionDirectory(pdir: PartitionDirectory);
     pub fn get_default_oid_from_partdesc(partdesc: PartitionDesc) -> Oid;
@@ -49768,7 +49772,7 @@ unsafe extern "C-unwind" {
     pub fn plpgsql_ns_find_nearest_loop(ns_cur: *mut PLpgSQL_nsitem) -> *mut PLpgSQL_nsitem;
     pub fn plpgsql_stmt_typename(stmt: *mut PLpgSQL_stmt) -> *const ::core::ffi::c_char;
     pub fn plpgsql_getdiag_kindname(kind: PLpgSQL_getdiag_kind::Type)
-    -> *const ::core::ffi::c_char;
+        -> *const ::core::ffi::c_char;
     pub fn plpgsql_mark_local_assignment_targets(func: *mut PLpgSQL_function);
     pub fn plpgsql_free_function_memory(func: *mut PLpgSQL_function);
     pub fn plpgsql_delete_callback(cfunc: *mut CachedFunction);
@@ -50215,7 +50219,7 @@ unsafe extern "C-unwind" {
     pub fn logicalrep_read_typ(in_: StringInfo, ltyp: *mut LogicalRepTyp);
     pub fn logicalrep_write_stream_start(out: StringInfo, xid: TransactionId, first_segment: bool);
     pub fn logicalrep_read_stream_start(in_: StringInfo, first_segment: *mut bool)
-    -> TransactionId;
+        -> TransactionId;
     pub fn logicalrep_write_stream_stop(out: StringInfo);
     pub fn logicalrep_write_stream_commit(
         out: StringInfo,
@@ -50538,7 +50542,7 @@ unsafe extern "C-unwind" {
         yyscanner: yyscan_t,
     ) -> ::core::ffi::c_int;
     pub fn replication_yylex(yylval_param: *mut YYSTYPE, yyscanner: yyscan_t)
-    -> ::core::ffi::c_int;
+        -> ::core::ffi::c_int;
     pub fn replication_yyerror(
         replication_parse_result_p: *mut *mut Node,
         yyscanner: yyscan_t,
@@ -50634,7 +50638,7 @@ unsafe extern "C-unwind" {
     pub fn AddInvertedQual(parsetree: *mut Query, qual: *mut Node);
     pub fn contain_aggs_of_level(node: *mut Node, levelsup: ::core::ffi::c_int) -> bool;
     pub fn locate_agg_of_level(node: *mut Node, levelsup: ::core::ffi::c_int)
-    -> ::core::ffi::c_int;
+        -> ::core::ffi::c_int;
     pub fn contain_windowfuncs(node: *mut Node) -> bool;
     pub fn locate_windowfunc(node: *mut Node) -> ::core::ffi::c_int;
     pub fn checkExprHasSubLink(node: *mut Node) -> bool;
@@ -50926,7 +50930,7 @@ unsafe extern "C-unwind" {
     ) -> ::core::ffi::c_int;
     pub fn FileSize(file: File) -> pgoff_t;
     pub fn FileTruncate(file: File, offset: pgoff_t, wait_event_info: uint32)
-    -> ::core::ffi::c_int;
+        -> ::core::ffi::c_int;
     pub fn FileWriteback(file: File, offset: pgoff_t, nbytes: pgoff_t, wait_event_info: uint32);
     pub fn FilePathName(file: File) -> *mut ::core::ffi::c_char;
     pub fn FileGetRawDesc(file: File) -> ::core::ffi::c_int;
@@ -55650,7 +55654,7 @@ unsafe extern "C-unwind" {
     pub fn get_ordering_op_for_equality_op(opno: Oid, use_lhs_type: bool) -> Oid;
     pub fn get_mergejoin_opfamilies(opno: Oid) -> *mut List;
     pub fn get_compatible_hash_operators(opno: Oid, lhs_opno: *mut Oid, rhs_opno: *mut Oid)
-    -> bool;
+        -> bool;
     pub fn get_op_hash_functions(
         opno: Oid,
         lhs_procno: *mut RegProcedure,
@@ -55829,7 +55833,7 @@ unsafe extern "C-unwind" {
     pub fn set_ps_display(activity: *const ::core::ffi::c_char);
     pub fn get_ps_display(displen: *mut ::core::ffi::c_int) -> *const ::core::ffi::c_char;
     pub fn format_procedure_extended(procedure_oid: Oid, flags: uint16)
-    -> *mut ::core::ffi::c_char;
+        -> *mut ::core::ffi::c_char;
     pub fn format_operator_extended(operator_oid: Oid, flags: uint16) -> *mut ::core::ffi::c_char;
     pub fn stringToQualifiedNameList(
         string: *const ::core::ffi::c_char,
